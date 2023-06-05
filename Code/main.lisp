ea8b46861a6e4e8be618f32d9d564f2f146924e0
Robert Strandh <robert.strandh@gmail.com>
2023-06-05 15:20:04 +0200

    Rename main entry point, and make it return an environment.

--- Code/main.lisp
+++ Code/main.lisp
@@ -1,13 +1,13 @@
 (cl:in-package #:common-boot)
 
-(defvar *environment*)
 
-(defun main ()
-  (setf *environment* (make-instance 'clb:run-time-environment))
-  (loop with cmd = (find-package '#:common-macro-definitions)
-        with cl-package = (find-package '#:common-lisp)
-        for symbol being each external-symbol in cmd
-        for symbol-name = (symbol-name symbol)
-        for cl-symbol = (find-symbol symbol-name cl-package)
-        do (setf (clo:macro-function nil *environment* cl-symbol)
-                 (macro-function symbol))))
+(defun create-environment ()
+  (let ((environment (make-instance 'clb:run-time-environment)))
+    (loop with cmd = (find-package '#:common-macro-definitions)
+          with cl-package = (find-package '#:common-lisp)
+          for symbol being each external-symbol in cmd
+          for symbol-name = (symbol-name symbol)
+          for cl-symbol = (find-symbol symbol-name cl-package)
+          do (setf (clo:macro-function nil environment cl-symbol)
+                   (macro-function symbol)))
+    environment))
