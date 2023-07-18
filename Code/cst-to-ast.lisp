2e05853cd79b12082a802cbdee5129d33776cfe6
Robert Strandh <robert.strandh@gmail.com>
2023-07-18 10:05:32 +0200

    Bind S-EXPRESSION-SYNTAX.EXPRESSION-GRAMMAR:*CLIENT*.

--- Code/cst-to-ast.lisp
+++ Code/cst-to-ast.lisp
@@ -2,7 +2,9 @@
 
 (defun cst-to-ast (client cst environment)
   (let ((*subforms-are-top-level-p* t)
-        (*compile-time-too* nil))
+        (*compile-time-too* nil)
+        (s-expression-syntax.expression-grammar:*client*
+          s-expression-syntax.concrete-syntax-tree::*client*))
     (convert client cst environment)))
 
 (defclass client (trucler-reference:client)
