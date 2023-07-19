666109416aaa61e1e121250f2c113bc2fc09c786
Robert Strandh <robert.strandh@gmail.com>
2023-07-19 07:57:01 +0200

    Define method on CONVERT-AST-IN-ENVIRONMENT.

--- Code/convert-ast.lisp
+++ Code/convert-ast.lisp
@@ -3,3 +3,7 @@
 (defmethod convert-ast (builder (ast ico:unparsed-form-ast))
   (with-builder-components (builder client environment)
     (convert client (ico:form ast) environment)))
+
+(defmethod convert-ast-in-environment
+    (client (ast ico:unparsed-form-ast) environment)
+  (convert client (ico:form ast) environment))
