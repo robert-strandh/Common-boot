44e43747adb47ff4ff889003ad742351cfd9617f
Robert Strandh <robert.strandh@gmail.com>
2023-07-20 12:27:33 +0200

    Change the tag class and return the AST.

--- Code/go.lisp
+++ Code/go.lisp
@@ -9,9 +9,11 @@
            (name (ico:name referencing-tag-ast))
            (description (describe-tag client environment name))
            (defining-tag-ast (trucler:identity description)))
+      (change-class referencing-tag-ast 'ico:tag-reference-ast)
       (reinitialize-instance referencing-tag-ast
         :tag-definition-ast defining-tag-ast)
       (reinitialize-instance defining-tag-ast
         :tag-reference-asts
         (append (ico:tag-reference-asts defining-tag-ast)
-                (list referencing-tag-ast))))))
+         (list referencing-tag-ast)))))
+  ast))
