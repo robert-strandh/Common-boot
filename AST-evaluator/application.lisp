0d910b8effa495f085ab8013672530170851b892
Robert Strandh <robert.strandh@gmail.com>
2023-07-21 14:07:01 +0200

    Define method on CPS for APPLICATON-AST.

--- AST-evaluator/application.lisp
+++ AST-evaluator/application.lisp
@@ -1,5 +1,27 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
-(defmethod cps (client (ast ico:appliation-ast) environment continuation)
-  nil)
-
+(defmethod cps (client (ast ico:application-ast) environment continuation)
+  (let* ((argument-asts (ico:argument-asts ast))
+         (function-variable (gensym))
+         (argument-variables
+           (loop for argument-ast in argument-asts
+                 collect (gensym)))
+         (action `(progn ,(push-stack client)
+                         (step (list ,argument-variables)
+                               ,function-variable))))
+    (loop for argument-ast in (reverse argument-asts)
+          for argument-variable in (reverse argument-variables)
+          do (setf action
+                   (cps client
+                        argument-ast
+                        environment
+                        `(lambda (&rest ,argument-variable)
+                           (setq ,argument-variable
+                                 (car ,argument-variable))
+                           ,action))))
+    (cps client
+         (ico:function-name-ast ast)
+         environment
+         `(lambda (&rest ,function-variable)
+            (setf ,function-variable (car ,function-variable))
+            ,action))))
