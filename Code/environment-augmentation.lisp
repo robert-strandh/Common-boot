ac3dac435a57c94206156db57cf6e6d2674f6fd9
Robert Strandh <robert.strandh@gmail.com>
2023-07-22 14:42:34 +0200

    Delete obsolete code and rewrite AUGMENT-ENVIRONMENT-WITH-VARIABLE.

--- Code/environment-augmentation.lisp
+++ Code/environment-augmentation.lisp
@@ -212,35 +212,17 @@
     (values (or already-globally-special-p declared-special-p)
             already-globally-special-p)))
 
-;;; Given a list of canonicalized declaration specifiers for a single
-;;; varible.  Return a type specifier resulting from all the type
-;;; declarations present in the list.
-(defun declared-type (declarations)
-  `(and ,@(loop for declaration in declarations
-                when (eq (cst:raw (cst:first declaration)) 'type)
-                  collect (cst:raw (cst:second declaration)))))
-
 ;;; Given a single variable bound by some binding form like LET or
 ;;; LET*, and a list of canonical declaration specifiers
 ;;; concerning that variable, return a new environment that contains
 ;;; information about that variable.
-;;;
-;;; ENVIRONMENT is the environment to be augmented.  If the binding form has
-;;; several bindings, it will contain entries for the variables
-;;; preceding the one that is currently treated.
-;;;
-;;; ORIGINAL-ENVIRONMENT is the environment in which we check whether
-;;; the variable is globally special.  For a LET form, this is the
-;;; environment in which the entire LET form was converted.  For a
-;;; LET* form, it is the same as ENVIRONMENT.
 (defun augment-environment-with-variable
-    (client variable-ast declarations environment original-environment)
+    (client variable-ast declaration-specifier-asts environment)
   (let ((new-env environment)
-        (name (ico:name variable-ast))
-        (raw-declarations (mapcar #'cst:raw declarations)))
+        (name (ico:name variable-ast)))
     (multiple-value-bind (special-p globally-p)
         (variable-is-special-p
-         client variable-ast declarations original-environment)
+         client variable-ast declaration-specifier-asts environment)
       (if special-p
           (unless globally-p
             (setf new-env
@@ -248,59 +230,28 @@
           (setf new-env
                 (trucler:add-lexical-variable
                  client new-env name variable-ast))))
-    (let ((type (declared-type declarations)))
-      (unless (equal type '(and))
-        (setf new-env
-              (trucler:add-variable-type client new-env name type))))
-    (when (member 'ignore raw-declarations :test #'eq :key #'car)
-      (setf new-env
-            (trucler:add-variable-ignore client new-env name 'ignore)))
-    (when (member 'ignorable raw-declarations :test #'eq :key #'car)
-      (setf new-env
-            (trucler:add-variable-ignore client new-env name 'ignorable)))
-    (when (member 'dynamic-extent raw-declarations :test #'eq :key #'car)
-      (setf new-env
-            (trucler:add-variable-dynamic-extent client new-env name)))
+    (loop for declaration-specifier-ast in declaration-specifier-asts
+          do (setf new-env
+                   (typecase declaration-specifier-ast
+                     (ico:type-ast
+                      ;; FIXME: Do nothing for now since it has not
+                      ;; been fully determined how type specifiers are
+                      ;; handled by the s-expression-syntax library.
+                      new-env)
+                     (ico:ignore-ast
+                      (trucler:add-variable-ignore
+                       client new-env name 'ignore))
+                     (ico:ignorable-ast
+                      (trucler:add-variable-ignore
+                       client new-env name 'ignorable))
+                     (ico:dynamic-extent-ast
+                      (trucler:add-variable-dynamic-extent
+                       client new-env name))
+                     (t
+                      new-env))))
     new-env))
-
-;;; The only purpose of this function is to call the function
-;;; AUGMENT-ENVIRONMENT-WITH-VARIABLE twice, once for the parameter
-;;; variable and once for its associated supplied-p parameter, except
-;;; that it also tests whether the supplied-p parameter is NIL,
-;;; indicating that no supplied-p parameter was given.  This function
-;;; returns the augmented environment.
-(defun augment-environment-with-parameter
-    (client var-cst supplied-p-cst dspecs environment)
-  (let ((new-env
-          (augment-environment-with-variable
-           client var-cst dspecs environment environment)))
-    (if (null supplied-p-cst)
-        new-env
-        (augment-environment-with-variable
-         client supplied-p-cst dspecs new-env new-env))))
 
 (defun augment-environment-with-local-function-name
     (client name-ast environment)
   (let* ((name (ico:name name-ast)))
     (trucler:add-local-function client environment name name-ast)))
-
-;;; Take an environment and a CST representing a single local function
-;;; definition.  Return a new environment which is like the one passed
-;;; as an argument, except the it has been augmented by the name of
-;;; the local function.
-(defun augment-environment-from-fdef (client environment definition-cst)
-  (let ((name-cst (cst:first definition-cst)))
-    (augment-environment-with-local-function-name client name-cst environment)))
-
-;;; Take an environment, a CST representing a list of function
-;;; definitions, and return a new environment which is like the one
-;;; passed as an argument, except that is has been augmented by the
-;;; local function names in the list.
-(defun augment-environment-from-fdefs (client environment definition-csts)
-  (loop with result = environment
-        for remaining = definition-csts then (cst:rest remaining)
-        until (cst:null remaining)
-        do (let ((definition-cst (cst:first remaining)))
-             (setf result
-                   (augment-environment-from-fdef client result definition-cst)))
-        finally (return result)))
