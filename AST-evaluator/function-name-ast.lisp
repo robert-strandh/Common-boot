7130da664ca9d3d79b2be16b268a4313387892ca
Robert Strandh <robert.strandh@gmail.com>
2023-07-26 15:45:50 +0200

    Remove incorrect code.

--- AST-evaluator/function-name-ast.lisp
+++ AST-evaluator/function-name-ast.lisp
@@ -1,10 +1,1 @@
 (cl:in-package #:common-boot-ast-evaluator)
-
-(defmethod cps
-    (client (ast ico:variable-reference-ast) environment continuation)
-  (let ((definition-ast (ico:variable-definition-ast ast)))
-    `(funcall ,continuation (list ,(lookup definition-ast environment)))))
-
-(defmethod cps
-    (client (ast ico:special-variable-reference-ast) environment continuation)
-  `(funcall ,continuation (list (fdefinition ,(ico:name ast)))))
