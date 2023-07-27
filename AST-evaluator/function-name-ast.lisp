1e727e6f87eddd85d07d252dbbfe213074d1517e
Robert Strandh <robert.strandh@gmail.com>
2023-07-27 10:23:46 +0200

    Supply methods for function names.

--- AST-evaluator/function-name-ast.lisp
+++ AST-evaluator/function-name-ast.lisp
@@ -1,1 +1,15 @@
 (cl:in-package #:common-boot-ast-evaluator)
+
+(defmethod cps
+    (client (ast ico:function-reference-ast) environment continuation)
+  (let ((definition-ast (ico:function-name-definition-ast ast)))
+    `(step (list ,(lookup definition-ast environment))
+           ,continuation)))
+
+(defmethod cps
+    (client
+     (ast ico:global-function-name-reference-ast)
+     environment
+     continuation)
+  ;; FIXME: refer to Clostrum instead.
+  `(step (list (fdefinition ,(ico:name ast))) ,continuation))
