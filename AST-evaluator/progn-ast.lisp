ee6151025f7e1984671e17c00fa474a6471b6c70
Robert Strandh <robert.strandh@gmail.com>
2023-07-27 08:50:42 +0200

    Bug fix: Put most code outside WHEN.

--- AST-evaluator/progn-ast.lisp
+++ AST-evaluator/progn-ast.lisp
@@ -4,18 +4,18 @@
   (let ((asts (reverse form-asts)))
     (when (null asts)
       (setf asts
-            (list (make-instance 'ico:literal-ast :literal nil)))
-      (let ((action (cps client (first asts) environment continuation)))
-        (loop for form-ast in (rest asts)
-              for argument-variable = (gensym)
-              do (setf action
-                       (cps client
-                            form-ast
-                            environment
-                            `(lambda (&rest ,argument-variable)
-                               (declare (ignore argument-variable))
-                               ,action))))
-        action))))
+            (list (make-instance 'ico:literal-ast :literal nil))))
+    (let ((action (cps client (first asts) environment continuation)))
+      (loop for form-ast in (rest asts)
+            for argument-variable = (gensym)
+            do (setf action
+                     (cps client
+                          form-ast
+                          environment
+                          `(lambda (&rest ,argument-variable)
+                             (declare (ignore argument-variable))
+                             ,action))))
+      action)))
 
 (defmethod cps (client (ast ico:progn-ast) environment continuation)
   (cps-implicit-progn client (ico:form-asts ast) environment continuation))
