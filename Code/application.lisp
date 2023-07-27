52e5f2351c34ca2de2171d09fe990f4b0545c467
Robert Strandh <robert.strandh@gmail.com>
2023-07-27 11:22:58 +0200

    Finalize the function name of an application.

--- Code/application.lisp
+++ Code/application.lisp
@@ -1,6 +1,9 @@
 (cl:in-package #:common-boot)
 
 (defmethod abp:finish-node (builder kind (ast ico:application-ast))
+  (with-builder-components (builder client environment)
+    (finalize-function-name-ast-from-environment
+     client (ico:function-name-ast ast) environment))
   ;; We assume that each argument is an unparsed form.
   (reinitialize-instance ast
     :argument-asts
