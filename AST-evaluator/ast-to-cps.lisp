0de95fb5d195c9e7e3d8250650d82b79cb2a1de8
Robert Strandh <robert.strandh@gmail.com>
2023-07-28 15:02:42 +0200

    Change signature of top-level function.

--- AST-evaluator/ast-to-cps.lisp
+++ AST-evaluator/ast-to-cps.lisp
@@ -3,7 +3,8 @@
 (defun ast-to-cps (client ast)
   (let ((variable (gensym))
         (environment (make-hash-table :test #'eq)))
-    `(lambda ()
+    `(lambda (client environment)
+       (declare (ignorable client environment))
        ,(cps client ast environment
              `(lambda (&rest ,variable)
                 (declare (ignore ,variable))
