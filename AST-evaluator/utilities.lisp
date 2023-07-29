4f010dd3c5d1db0aa8c4638ed8345f18ba4fe0b0
Robert Strandh <robert.strandh@gmail.com>
2023-07-29 18:16:19 +0200

    Define function for importing a function from the host.

--- AST-evaluator/utilities.lisp
+++ AST-evaluator/utilities.lisp
@@ -5,3 +5,11 @@
 
 (defun (setf lookup) (value ast environment)
   (setf (gethash ast environment) value))
+
+(defun import-host-function (client name global-environment)
+  (let ((host-function (fdefinition name)))
+    (setf (clostrum:fdefinition client global-environment name)
+          (lambda (&rest arguments)
+            (setf *arguments*
+                  (multiple-value-list (apply host-function arguments)))
+            (pop-stack)))))
