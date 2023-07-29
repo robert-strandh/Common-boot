eb03c64b59b2f9c79c01f9c52af9b41480c7d54d
Robert Strandh <robert.strandh@gmail.com>
2023-07-29 11:29:07 +0200

    Introduce package-local nickname CM for COMMON-MACROS.

--- AST-evaluator/packages.lisp
+++ AST-evaluator/packages.lisp
@@ -1,6 +1,8 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:common-boot-ast-evaluator
-  (:local-nicknames (#:ico #:iconoclast))
+  (:local-nicknames
+   (#:ico #:iconoclast)
+   (#:cm #:common-macros))
   (:use #:common-lisp)
   (:shadow #:symbol-value #:step))
