4e7f7127b210f9efca8bf7fda93984eed9825cc7
Robert Strandh <robert.strandh@gmail.com>
2023-07-30 10:22:53 +0200

    Use new utility macro WITH-DEFAULT-PARAMETERS.

--- Test/application.lisp
+++ Test/application.lisp
@@ -3,10 +3,7 @@
 (define-test application)
 
 (define-test application-no-arguments
-  (let* ((environment (cb:create-environment))
-         (client (make-instance 'trucler-reference:client))
-         (global-environment
-           (trucler:global-environment client environment)))
+  (with-default-parameters (client environment global-environment)
     (cbae:import-host-function client '+ global-environment)
     (is #'equal
         (multiple-value-list (+))
