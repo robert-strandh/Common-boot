bb40211327a728f409567e92675c7f1c6f8a1a70
Robert Strandh <robert.strandh@gmail.com>
2023-07-30 12:23:23 +0200

    Improve how QUOTE is handled.

--- AST-evaluator/quote-ast.lisp
+++ AST-evaluator/quote-ast.lisp
@@ -2,5 +2,5 @@
 
 (defmethod cps (client (ast ico:quote-ast) environment continuation)
   ;; This seems wrong.  Fix Iconoclast.
-  (ico:form (ico:object-ast ast)))
-
+  `(step (list ,(ico:form (ico:object-ast ast)))
+         ,continuation))
