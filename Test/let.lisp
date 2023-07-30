051d5b358a5a2e26a5d5c9d0d5fdbab140c5f8fb
Robert Strandh <robert.strandh@gmail.com>
2023-07-30 11:19:18 +0200

    Add test for nested LETs.

--- Test/let.lisp
+++ Test/let.lisp
@@ -22,3 +22,12 @@
     (is #'equal
         (multiple-value-list #1=(let ((x 234)) x))
         (cbae:eval-expression '#1# environment))))
+
+(define-test let-nested-binding
+  :parent let
+  (with-default-parameters (client environment global-environment)
+    (cbae:import-host-function client '+ global-environment)
+    (is #'equal
+        (multiple-value-list
+         #1=(let ((x 234)) (let ((x 345) (y x)) (+ x y))))
+        (cbae:eval-expression '#1# environment))))
