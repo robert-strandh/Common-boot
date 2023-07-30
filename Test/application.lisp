a8379126fc5bacc6600eb310839c03d4ba6e8959
Robert Strandh <robert.strandh@gmail.com>
2023-07-30 10:34:39 +0200

    Add more tests for applications.

--- Test/application.lisp
+++ Test/application.lisp
@@ -8,3 +8,24 @@
     (is #'equal
         (multiple-value-list #1=(+))
         (cbae:eval-expression '#1# environment))))
+
+(define-test application-one-argument
+  (with-default-parameters (client environment global-environment)
+    (cbae:import-host-function client '+ global-environment)
+    (is #'equal
+        (multiple-value-list #1=(+ 234))
+        (cbae:eval-expression '#1# environment))))
+
+(define-test application-two-argument
+  (with-default-parameters (client environment global-environment)
+    (cbae:import-host-function client '+ global-environment)
+    (is #'equal
+        (multiple-value-list #1=(+ 234 345))
+        (cbae:eval-expression '#1# environment))))
+
+(define-test application-multiple-return-values
+  (with-default-parameters (client environment global-environment)
+    (cbae:import-host-function client 'floor global-environment)
+    (is #'equal
+        (multiple-value-list #1=(floor 234 33))
+        (cbae:eval-expression '#1# environment))))
