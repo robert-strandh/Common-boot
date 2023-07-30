fed03741a7cf7aa60da881d952d6e7cd7bb792f9
Robert Strandh <robert.strandh@gmail.com>
2023-07-30 10:13:55 +0200

    Introduce package-local nicknames for COMMON-BOOT and AST evaluator.

--- Test/packages.lisp
+++ Test/packages.lisp
@@ -1,4 +1,7 @@
 (cl:in-package #:common-lisp-user)
 
 (defpackage #:common-boot-test
-  (:use #:common-lisp #:parachute))
+  (:use #:common-lisp #:parachute)
+  (:local-nicknames (#:cb #:common-boot)
+                    (#:cbae #:common-boot-ast-evaluator)))
+
