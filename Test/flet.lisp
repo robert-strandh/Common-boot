04398413ea47adbf12eafb38943c9c7e9ea255ec
Robert Strandh <robert.strandh@gmail.com>
2023-07-31 06:47:00 +0200

    Add more tests for FLET.

--- Test/flet.lisp
+++ Test/flet.lisp
@@ -9,3 +9,23 @@
         (multiple-value-list
          #1=(flet ((f () 234)) (f)))
         (cbae:eval-expression '#1# environment))))
+
+(define-test flet-one-parameters
+  :parent flet
+  (with-default-parameters (client environment global-environment)
+    (is #'equal
+        (multiple-value-list
+         #1=(flet ((f (x) x)) (f 234)))
+        (cbae:eval-expression '#1# environment))))
+
+(define-test flet-nested
+  :parent flet
+  (with-default-parameters (client environment global-environment)
+    (cbae:import-host-function client '+ global-environment)
+    (is #'equal
+        (multiple-value-list
+         #1=(flet ((f (x) (+ x 1)))
+              (flet ((f (x) x)
+                     (g (x) (f x)))
+                (g 234))))
+        (cbae:eval-expression '#1# environment))))
