73f2edb6d1d782832572a3cc330e6ad3fae61c61
Robert Strandh <robert.strandh@gmail.com>
2023-07-31 05:57:58 +0200

    Improve code layout.

--- Test/catch-and-throw.lisp
+++ Test/catch-and-throw.lisp
@@ -16,5 +16,5 @@
     (cbae:import-host-function client '+ global-environment)
     (is #'equal
         (multiple-value-list
-         #1=(catch 'hello (throw 'hello  234) (+ 3 4)))
+         #1=(catch 'hello (throw 'hello 234) (+ 3 4)))
         (cbae:eval-expression '#1# environment))))
