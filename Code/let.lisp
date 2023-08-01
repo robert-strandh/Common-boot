89647d6bb63d493496d476c9a887cedd2873b6a0
Robert Strandh <robert.strandh@gmail.com>
2023-08-01 11:48:56 +0200

    Rewrite method for LET using new augmentation function.

--- Code/let.lisp
+++ Code/let.lisp
@@ -1,42 +1,10 @@
 (cl:in-package #:common-boot)
 
-;;; BUILDER is a builder to use for converting the value forms in the
-;;; binding, ENVIRONMENT is the environment to augment with the
-;;; variable of the binding.  The augmented environment is returned.
-(defgeneric finalize-binding
-    (client builder environment binding-ast declaration-asts))
-
 ;;; FIXME: this function is very similar to
 ;;; AUGMENT-ENVIRONMENT-WITH-VARIABLE.
 (defun augment-environment-with-binding-variable
     (client environment variable-name-ast declaration-asts)
   (let ((variable-name (ico:name variable-name-ast)))
-    (multiple-value-bind (special-p globally-special-p)
-        (variable-is-special-p
-         client
-         variable-name-ast
-         declaration-asts
-         environment)
-      (change-class variable-name-ast
-                    (if special-p
-                        'ico:special-variable-bound-ast
-                        'ico:variable-definition-ast))
-      (if special-p
-          (unless globally-special-p
-            (trucler:add-local-special-variable
-             client environment variable-name))
-          (trucler:add-lexical-variable
-           client environment
-           variable-name variable-name-ast)))))
-
-
-(defmethod finalize-binding
-    (client builder environment (ast ico:variable-binding-ast) declaration-asts)
-  (let* ((form-ast (ico:form-ast ast))
-         (variable-name-ast (ico:variable-name-ast ast))
-         (variable-name (ico:name variable-name-ast)))
-    (reinitialize-instance ast
-      :form-ast (convert-ast builder form-ast))
     (multiple-value-bind (special-p globally-special-p)
         (variable-is-special-p
          client
@@ -62,12 +30,15 @@
     (with-builder-components (builder client environment)
       (let ((new-environment environment))
         (loop for binding-ast in (ico:binding-asts ast)
-              do (setf new-environment
-                       (finalize-binding
+              for variable-name-ast = (ico:variable-name-ast binding-ast)
+              for form-ast = (ico:form-ast ast)
+              do (reinitialize-instance binding-ast
+                   :form-ast (convert-ast builder form-ast))
+                 (setf new-environment
+                       (augment-environment-with-binding-variable
                         client
-                        builder
                         new-environment
-                        binding-ast
+                        variable-name-ast
                         (ico:declaration-asts ast))))
         (let ((new-builder (make-builder client new-environment)))
               (reinitialize-instance ast
