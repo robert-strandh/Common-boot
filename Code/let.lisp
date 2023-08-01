f624354ebf63d9da05158e1b130b05ec86cb33f3
Robert Strandh <robert.strandh@gmail.com>
2023-08-01 11:43:10 +0200

    Define function AUGMENT-ENVIRONMENT-WITH-VARIABLE.

--- Code/let.lisp
+++ Code/let.lisp
@@ -6,6 +6,30 @@
 (defgeneric finalize-binding
     (client builder environment binding-ast declaration-asts))
 
+;;; FIXME: this function is very similar to
+;;; AUGMENT-ENVIRONMENT-WITH-VARIABLE.
+(defun augment-environment-with-binding-variable
+    (client environment variable-name-ast declaration-asts)
+  (let ((variable-name (ico:name variable-name-ast)))
+    (multiple-value-bind (special-p globally-special-p)
+        (variable-is-special-p
+         client
+         variable-name-ast
+         declaration-asts
+         environment)
+      (change-class variable-name-ast
+                    (if special-p
+                        'ico:special-variable-bound-ast
+                        'ico:variable-definition-ast))
+      (if special-p
+          (unless globally-special-p
+            (trucler:add-local-special-variable
+             client environment variable-name))
+          (trucler:add-lexical-variable
+           client environment
+           variable-name variable-name-ast)))))
+
+
 (defmethod finalize-binding
     (client builder environment (ast ico:variable-binding-ast) declaration-asts)
   (let* ((form-ast (ico:form-ast ast))
