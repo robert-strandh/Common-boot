b71e12a0c91f644aafccaca39eb4b7374d3878e8
Robert Strandh <robert.strandh@gmail.com>
2023-08-01 10:22:58 +0200

    Simplify method for LET* by using new function FINALIZE-BINDING.

--- Code/letstar.lisp
+++ Code/letstar.lisp
@@ -5,39 +5,12 @@
     ((builder builder)
      (kind t)
      (ast ico:let*-ast))
-  (with-accessors ((binding-asts ico:binding-asts)
-                   (declaration-asts ico:declaration-asts))
-      ast
-    (with-builder-components (builder client environment)
-      (let ((new-environment environment)
-            (new-builder builder))
-        (loop for binding-ast in binding-asts
-              for form-ast = (ico:form-ast binding-ast)
-              for variable-name-ast = (ico:variable-name-ast binding-ast)
-              for variable-name = (ico:name variable-name-ast)
-              do (reinitialize-instance binding-ast
-                   :form-ast (convert-ast new-builder form-ast))
-                 (multiple-value-bind (special-p globally-special-p)
-                     (variable-is-special-p
-                      client
-                      variable-name-ast
-                      declaration-asts
-                      new-environment)
-                   (change-class variable-name-ast
-                                 (if special-p
-                                     'ico:special-variable-bound-ast
-                                     'ico:variable-definition-ast))
-                   (setf new-environment
-                         (if special-p
-                             (unless globally-special-p
-                               (trucler:add-local-special-variable
-                                client new-environment variable-name))
-                             (trucler:add-lexical-variable
-                              client new-environment
-                              variable-name variable-name-ast))))
-                 (setf new-builder
-                       (make-builder client new-environment)))
-        (reinitialize-instance ast
-          :form-asts
-          (loop for body-ast in (ico:form-asts ast)
-                collect (convert-ast new-builder body-ast)))))))
+  (let ((new-builder builder))
+    (loop for binding-ast in (ico:binding-asts ast)
+          do (setf new-builder
+                   (finalize-binding
+                    new-builder binding-ast (ico:declaration-asts ast))))
+    (reinitialize-instance ast
+      :form-asts
+      (loop for body-ast in (ico:form-asts ast)
+            collect (convert-ast new-builder body-ast)))))
