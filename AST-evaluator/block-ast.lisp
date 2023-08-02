75406907471e71a95ab6cd60f47b4ae69b6f8922
Robert Strandh <robert.strandh@gmail.com>
2023-08-02 14:36:16 +0200

    Use a generated symbol for the block name.

--- AST-evaluator/block-ast.lisp
+++ AST-evaluator/block-ast.lisp
@@ -1,9 +1,11 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client (ast ico:block-ast) environment continuation)
-  `(progn (push (make-instance 'block-entry
-                  :name ',(ico:name (ico:name-ast ast))
-                  :continuation ,continuation)
-                *dynamic-environment*)
-          ,(cps-implicit-progn
-            client (ico:form-asts ast) environment continuation)))
+  (let ((name (gensym)))
+    (setf (lookup ast environment) name)
+    `(progn (push (make-instance 'block-entry
+                    :name ',name
+                    :continuation ,continuation)
+                  *dynamic-environment*)
+            ,(cps-implicit-progn
+              client (ico:form-asts ast) environment continuation))))
