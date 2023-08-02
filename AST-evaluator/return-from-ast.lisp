57074e31b2f110ccf77bc54279d90d2fc8686bb4
Robert Strandh <robert.strandh@gmail.com>
2023-08-02 14:49:32 +0200

    Use the environment to look up the block name.

--- AST-evaluator/return-from-ast.lisp
+++ AST-evaluator/return-from-ast.lisp
@@ -1,9 +1,11 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client (ast ico:return-from-ast) environment continuation)
-  (let ((form-ast (ico:form-ast ast))
-        (name (ico:name (ico:name-ast ast)))
-        (temp (gensym)))
+  (let* ((form-ast (ico:form-ast ast))
+         (block-name-ast (ico:name-ast ast))
+         (name (lookup (ico:block-name-definition-ast block-name-ast)
+                       environment))
+         (temp (gensym)))
     (cps client
          (if (null form-ast)
              (make-instance 'ico:literal-ast :literal nil)
