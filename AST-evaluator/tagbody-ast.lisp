4dcf086c209e8b5544fd52a7e6b8fec5ff604490
Robert Strandh <robert.strandh@gmail.com>
2023-08-02 09:35:55 +0200

    Pop the dynamic environment before leaving the TAGBODY.

--- AST-evaluator/tagbody-ast.lisp
+++ AST-evaluator/tagbody-ast.lisp
@@ -1,9 +1,16 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client (ast ico:tagbody-ast) environment continuation)
-  (let ((temp (gensym))
-        (action `(step (list nil) ,continuation)))
-    (loop for segment-ast in (ico:segment-asts ast)
+  (let* ((temp (gensym))
+         (segment-asts (ico:segment-asts ast))
+         (label-count (- (length segment-asts)
+                         (if (and (not (null segment-asts))
+                                  (null (ico:tag-ast (first segment-asts))))
+                             1 0)))
+         (action `(progn
+                    (loop repeat ,label-count do (pop *dynamic-environment*))
+                    (step (list nil) ,continuation))))
+    (loop for segment-ast in segment-asts
           for tag-ast = (ico:tag-ast segment-ast)
           for new-continuation = `(lambda (&rest ,temp)
                                     (declare (ignore ,temp))
