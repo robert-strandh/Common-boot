c2d3d5817f2754c9015040095e6e71c58b21a20f
Robert Strandh <robert.strandh@gmail.com>
2023-08-07 16:57:01 +0200

    Rewrite CPS method for application.

--- AST-evaluator/application-ast.lisp
+++ AST-evaluator/application-ast.lisp
@@ -2,27 +2,33 @@
 
 (defmethod cps (client (ast ico:application-ast) environment continuation)
   (let* ((argument-asts (ico:argument-asts ast))
-         (function-variable (gensym))
+         (function-variable (gensym "FUN-"))
          (argument-variables
            (loop for argument-ast in argument-asts
-                 collect (gensym "ARG")))
-         (action `(progn (setf *continuation* ,continuation)
-                         ,(push-stack-operation client)
-                         (step (list ,@argument-variables)
-                               ,function-variable))))
-    (loop for argument-ast in (reverse argument-asts)
-          for argument-variable in (reverse argument-variables)
-          do (setf action
-                   (cps client
-                        argument-ast
-                        environment
-                        `(lambda (&rest ,argument-variable)
-                           (setq ,argument-variable
-                                 (car ,argument-variable))
-                           ,action))))
-    (cps client
-         (ico:function-name-ast ast)
-         environment
-         `(lambda (&rest ,function-variable)
-            (setf ,function-variable (car ,function-variable))
-            ,action))))
+                 collect (gensym "ARG-")))
+         (name (gensym "C-")))
+    `(let ,(cons function-variable argument-variables)
+       (let* ((,name
+                (lambda (&rest ignore)
+                  (declare (ignore ignore))
+                  (step (list ,@argument-variables) ,function-variable)))
+              ,@(loop for argument-ast in (reverse argument-asts)
+                      for argument-variable in (reverse argument-variables)
+                      collect `(,name (lambda (&rest var)
+                                        (setq ,argument-variable (car var))
+                                        (step nil ,name)))
+                      collect `(,name (lambda (&rest ignore)
+                                        (declare (ignore ignore))
+                                        ,(cps client
+                                              argument-ast
+                                              environment
+                                              name))))
+              (,name (lambda (&rest var)
+                       (setf ,function-variable (car var))
+                       (step nil ,name))))
+         (setq *continuation* ,continuation)
+         (push-stack)
+         ,(cps client
+               (ico:function-name-ast ast)
+               environment
+               name)))))
