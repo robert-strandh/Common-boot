d96860fda68867218969afcbac98488c6bca1eb1
Robert Strandh <robert.strandh@gmail.com>
2023-08-08 12:34:07 +0200

    Fix typo.

--- Test/flet.lisp
+++ Test/flet.lisp
@@ -10,7 +10,7 @@
          #1=(flet ((f () 234)) (f)))
         (cbae:eval-expression '#1# environment))))
 
-(define-test flet-one-parameters
+(define-test flet-one-parameter
   :parent flet
   (with-default-parameters (client environment global-environment)
     (is #'equal
