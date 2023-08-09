76fdcb36536fb224b0f25794912ed5ac50c04b9b
Robert Strandh <robert.strandh@gmail.com>
2023-08-09 12:04:11 +0200

    Convert body forms as well.

--- Code/with-input-from-string.lisp
+++ Code/with-input-from-string.lisp
@@ -17,4 +17,5 @@
           :string-ast (convert-ast new-builder (ico:string-ast ast))
           :index-ast (convert-optional-ast new-builder (ico:index-ast ast))
           :start-ast (convert-optional-ast new-builder (ico:start-ast ast))
-          :end-ast (convert-optional-ast new-builder (ico:end-ast ast)))))))
+          :end-ast (convert-optional-ast new-builder (ico:end-ast ast))
+          :form-asts (convert-asts new-builder (ico:form-asts ast)))))))
