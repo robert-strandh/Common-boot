0649a40f5ddd878c122df6b9e5ffef79dbb79fcd
Robert Strandh <robert.strandh@gmail.com>
2023-08-11 16:58:46 +0200

    Add test for simple BLOCK with RETURN-FROM.

--- Test/block-and-return-from.lisp
+++ Test/block-and-return-from.lisp
@@ -1,13 +1,19 @@
 (cl:in-package #:common-boot-test)
 
-(define-test block-and-return-from
-  :skip (block-with-return-from-inside-let))
+(define-test block-and-return-from)
 
 (define-test block-no-return-from
   :parent block-and-return-from
   (with-default-parameters (client environment global-environment)
     (is #'equal
         (multiple-value-list #1=(block b 234))
+        (cbae:eval-expression '#1# environment))))
+
+(define-test block-with-return-from
+  :parent block-and-return-from
+  (with-default-parameters (client environment global-environment)
+    (is #'equal
+        (multiple-value-list #1=(block b (return-from b 345) 234))
         (cbae:eval-expression '#1# environment))))
 
 (define-test block-with-return-from-inside-let
