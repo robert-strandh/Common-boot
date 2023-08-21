b8518a90c317147972af90c6321105207ce0cb2c
Robert Strandh <robert.strandh@gmail.com>
2023-08-21 09:12:08 +0200

    Introduce variable *HOST-NAMES* for hash table during translation.

--- AST-evaluator/utilities.lisp
+++ AST-evaluator/utilities.lisp
@@ -1,10 +1,14 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
-(defun lookup (ast environment)
-  (gethash ast environment))
+;;; During CPS creation, this variable holds a hash table that maps
+;;; target objects such as variables and tags to host names.
+(defvar *host-names*)
 
-(defun (setf lookup) (value ast environment)
-  (setf (gethash ast environment) value))
+(defun lookup (ast)
+  (gethash ast *host-names*))
+
+(defun (setf lookup) (value ast)
+  (setf (gethash ast *host-names*) value))
 
 (defun import-host-function (client name global-environment)
   (let ((host-function (fdefinition name)))
