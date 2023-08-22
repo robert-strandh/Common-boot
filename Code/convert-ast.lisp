554e3fea698dc2825bcdf6dbb1951a74b3cfc9e6
Robert Strandh <robert.strandh@gmail.com>
2023-08-22 15:58:41 +0200

    Define default method on CONVERT-AST.

--- Code/convert-ast.lisp
+++ Code/convert-ast.lisp
@@ -1,5 +1,10 @@
 (cl:in-package #:common-boot)
 
+;;; This method is applicable if the AST we are given has already been
+;;; converted, so nothing needs to be done.
+(defmethod convert-ast (builder ast)
+  (declare (ignorable builder ast)))
+
 (defmethod convert-ast (builder (ast ico:unparsed-form-ast))
   (with-builder-components (builder client environment)
     (convert client (ico:form ast) environment)))
