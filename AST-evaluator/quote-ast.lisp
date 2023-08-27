e9abbce3bc7db1879787680820d83eea650acb19
Robert Strandh <robert.strandh@gmail.com>
2023-08-27 12:57:42 +0200

    Call accessor FORM on the UNPARSED-FORM-AST.

--- AST-evaluator/quote-ast.lisp
+++ AST-evaluator/quote-ast.lisp
@@ -2,5 +2,5 @@
 
 ;;; The use of CST:RAW seems wrong here. 
 (defmethod cps (client (ast ico:quote-ast) continuation)
-  `(step (list ',(cst:raw (ico:object-ast ast)))
+  `(step (list ',(cst:raw (ico:form (ico:object-ast ast))))
          ,continuation))
