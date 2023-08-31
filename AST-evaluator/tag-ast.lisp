14227b772d00e55e92f81d6ebec56b7e6dbfed45
Robert Strandh <robert.strandh@gmail.com>
2023-08-31 14:31:04 +0200

    Call CST:RAW to get to the raw tag

--- AST-evaluator/tag-ast.lisp
+++ AST-evaluator/tag-ast.lisp
@@ -1,5 +1,5 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client (ast ico:tag-ast) continuation)
-  `(step (list ,(ico:name ast))
+  `(step (list ,(cst:raw (ico:name ast)))
          ,continuation))
