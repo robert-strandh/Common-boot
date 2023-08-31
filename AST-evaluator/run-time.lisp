e4c356f26aefd83991e4f2865548a327e71c139a
Robert Strandh <robert.strandh@gmail.com>
2023-08-31 14:29:26 +0200

    Factor code.  Define function DO-THROW.

--- AST-evaluator/run-time.lisp
+++ AST-evaluator/run-time.lisp
@@ -21,20 +21,22 @@
     :reader continuation)
    ))
 
-(defclass block-entry (dynamic-environment-entry valid-p-mixin)
+(defclass block-entry (continuation-entry valid-p-mixin)
   ((%name :initarg :name :reader name)))
 
 (defparameter *dynamic-environment* '())
 
-(defun do-return-from (name)
-  (loop for rest on *dynamic-environment*
+;;; FIXME: there is some considerable code duplication here.
+
+(defun do-return-from (name dynamic-environment)
+  (loop for rest on dynamic-environment
         for entry = (first rest)
         do (when (and (typep entry 'block-entry)
                       (eq name (name entry)))
              (if (valid-p entry)
                  ;; FIXME: handle UNWIND-PROTECT.
                  (progn 
-                   (loop for entry-to-invalidate in *dynamic-environment*
+                   (loop for entry-to-invalidate in dynamic-environment
                          until (eq entry-to-invalidate entry)
                          do (setf (valid-p entry-to-invalidate) nil))
                    (setf *continuation* (continuation entry))
@@ -46,11 +48,8 @@
                         entry)))
         finally (error "No valid block entry for ~s" name)))
 
-(defclass tag-entry (dynamic-environment-entry valid-p-mixin)
-  ((%name :initarg :name :reader name)
-   (%continuation
-    :initarg :continuation
-    :reader continuation)))
+(defclass tag-entry (continuation-entry valid-p-mixin)
+  ((%name :initarg :name :reader name)))
 
 (defun do-go (name dynamic-environment)
   (loop for rest on dynamic-environment
@@ -72,8 +71,28 @@
                         entry)))
         finally (error "no valid tag entry for ~s" name)))
 
-(defclass catch-entry (continuation-entry)
+(defclass catch-entry (continuation-entry valid-p-mixin)
   ((%tag :initarg :tag :reader tag)))
+
+(defun do-throw (tag dynamic-environment)
+  (loop for rest on dynamic-environment
+        for entry = (first rest)
+        do (when (and (typep entry 'catch-entry)
+                      (eq tag (tag entry)))
+             (if (valid-p entry)
+                 ;; FIXME: handle UNWIND-PROTECT.
+                 (progn 
+                   (loop for entry-to-invalidate in dynamic-environment
+                         until (eq entry-to-invalidate entry)
+                         do (setf (valid-p entry-to-invalidate) nil))
+                   (setf *continuation* (continuation entry))
+                   (setf *stack* (stack entry))
+                   (return))
+                 ;; For now, signal a host error.  It would be better
+                 ;; to call the target function ERROR.
+                 (error "attempt to use an expired entry ~s~%"
+                        entry)))
+        finally (error "no valid tag entry for ~s" tag)))
 
 (defclass unwind-protect-entry (dynamic-environment-entry)
   ())
@@ -111,8 +130,8 @@
   (declare (ignore client))
   `(pop-stack))
 
-(defun symbol-value (client name cell)
-  (loop for entry in *dynamic-environment*
+(defun symbol-value (client name cell dynamic-environment)
+  (loop for entry in dynamic-environment
         do (when (and (typep entry 'special-variable-entry)
                       (eq name (name entry)))
              (return (value entry)))
