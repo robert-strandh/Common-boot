4c2ec56a93ebb869227b7fdb87e56d6580491f3e
Robert Strandh <robert.strandh@gmail.com>
2023-08-31 08:17:03 +0200

    Simplify CPS method for GO by calling new function DO-GO.

--- AST-evaluator/go-ast.lisp
+++ AST-evaluator/go-ast.lisp
@@ -1,11 +1,6 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client (ast ico:go-ast) continuation)
-  (let* ((tag-ast (ico:tag-ast ast))
-         (name (lookup (ico:tag-definition-ast tag-ast))))
-    `(loop for entry in *dynamic-environment*
-           do (when (and (typep entry 'block-entry)
-                         (eq ',name (name entry)))
-                ;; FIXME: check for expired entry.
-                (step '()
-                      (continuation entry))))))
+  (let* ((tag-reference-ast (ico:tag-ast ast))
+         (name (lookup (ico:tag-definition-ast tag-reference-ast))))
+    `(do-go ,name dynamic-environment)))
