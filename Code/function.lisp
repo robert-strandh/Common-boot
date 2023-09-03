a105bb7ac85df0b1102691f82552f6f46a0d85a8
Robert Strandh <robert.strandh@gmail.com>
2023-09-03 09:13:30 +0200

    Handle case where argument of FUNCTION is a LAMBDA expression.

--- Code/function.lisp
+++ Code/function.lisp
@@ -1,7 +1,18 @@
 (cl:in-package #:common-boot)
 
 (defmethod abp:finish-node (builder kind (ast ico:function-ast))
-  (with-builder-components (builder client environment)
-    (finalize-function-name-ast-from-environment
-     client (ico:name-ast ast) environment))
+  (let ((name-ast (ico:name-ast ast)))
+    (with-builder-components (builder client environment)
+      (if (typep name-ast 'ico:lambda-expression-ast)
+          (let ((body-environment
+                  (finalize-lambda-list
+                   client environment (ico:lambda-list-ast name-ast))))
+            (reinitialize-instance name-ast
+              :form-asts
+              (loop for form-ast in (ico:form-asts name-ast)
+                    collect
+                    (convert-ast-in-environment
+                     client form-ast body-environment))))
+          (finalize-function-name-ast-from-environment
+           client (ico:name-ast ast) environment))))
   ast)
