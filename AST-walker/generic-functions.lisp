c2843609bc9599d03db31b0354f99c7fb49e190c
Robert Strandh <robert.strandh@gmail.com>
2023-09-18 10:25:53 +0200

    Remove PRE-PROCESS and POST-PROCESS parameters.

--- AST-walker/generic-functions.lisp
+++ AST-walker/generic-functions.lisp
@@ -1,35 +1,34 @@
 (cl:in-package #:common-boot-ast-walker)
 
-(defgeneric walk-ast-node (client node pre-process post-process))
+(defgeneric walk-ast-node (client node))
 
-(defmethod walk-ast-node (client node pre-process post-process)
+(defmethod walk-ast-node (client node)
   node)
 
-(defmethod walk-ast-node
-    (client (ast iconoclast:ast) pre-process post-process)
+(defmethod walk-ast-node (client (ast iconoclast:ast))
   (loop for (cardinality slot-reader) in (iconoclast:slot-designators ast)
         do (ecase cardinality
              (*
               (loop for child in (funcall slot-reader ast)
-                    do (walk-ast-node client child pre-process post-process)))
+                    do (walk-ast-node client child)))
              (iconoclast:?
               (let ((possible-child (funcall slot-reader ast)))
                 (unless (null possible-child)
-                  (walk-ast-node client possible-child pre-process post-process))))
+                  (walk-ast-node client possible-child))))
              (1
               (let ((child (funcall slot-reader ast)))
-                (walk-ast-node client child pre-process post-process)))))
+                (walk-ast-node client child)))))
   ast)
 
 (defvar *visited*)
 
-(defmethod walk-ast-node :around (client node pre-process post-process)
+(defmethod walk-ast-node :around (client node)
   (unless (gethash node *visited*)
     (setf (gethash node *visited*) t)
     (call-next-method)))
 
-(defgeneric walk-ast (client ast pre-process post-process))
+(defgeneric walk-ast (client ast))
 
-(defmethod walk-ast (client ast pre-process post-process)
+(defmethod walk-ast (client ast)
   (let ((*visited* (make-hash-table :test #'eq)))
-    (walk-ast-node client ast pre-process post-process)))
+    (walk-ast-node client ast)))
