828e36784ffe3cc0da25fb4348f05dffce596ce2
Robert Strandh <robert.strandh@gmail.com>
2023-09-18 15:05:42 +0200

    Define generic function CANONICALIZE-DECLARATION-SPECIFIER-AST.

--- AST-transformations/canonicalize-declaration-asts.lisp
+++ AST-transformations/canonicalize-declaration-asts.lisp
@@ -1,10 +1,18 @@
 (cl:in-package #:common-boot-ast-transformations)
 
-(defgeneric canonicalize-declaration-asts (client ast))
+(defgeneric canonicalize-declaration-specifier-ast
+    (client declaration-specifier-ast))
+
+(defmethod canonicalize-declaration-specifier-ast
+    (client declaration-specifier-ast))
 
 (defmethod cbaw:walk-ast-node :around
     ((client client) (ast ico:declaration-asts-mixin))
   (reinitialize-instance ast
     :declaration-asts
-    (canonicalize-declaration-asts client (ico:declaration-asts ast)))
+    (loop for declaration-ast in (ico:declaration-asts ast)
+          append (loop for declaration-specifier-ast
+                         in (ico:declaration-specifier-asts declaration-ast)
+                       append (canonicalize-declaration-specifier-ast
+                               declaration-specifier-ast))))
   (call-next-method))
