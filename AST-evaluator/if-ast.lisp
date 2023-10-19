343381f36d40ef5971d160e22f164575fb906809
Robert Strandh <robert.strandh@gmail.com>
2023-10-19 12:44:36 +0200

    Introduce a variable for the continuation.

--- AST-evaluator/if-ast.lisp
+++ AST-evaluator/if-ast.lisp
@@ -1,20 +1,22 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
+;;; FIXME: remove new-continuaton.
 (defmethod cps (client (ast ico:if-ast) continuation)
   (let ((temp (gensym "TEST"))
         (new-continuation (gensym "C-"))
+        (continuation-variable (gensym "C-"))
         (else-ast (if (null (ico:else-ast ast))
                       (make-instance 'ico:literal-ast :literal nil)
                       (ico:else-ast ast))))
-    (cps client
-         (ico:test-ast ast)
-         `(let ((,new-continuation ,continuation))
-            (lambda (&rest ,temp)
-              (setq ,temp (car ,temp))
-              (if (null ,temp)
-                  ,(cps client
-                        else-ast
-                        new-continuation)
-                  ,(cps client
-                        (ico:then-ast ast)
-                        new-continuation)))))))
+    `(let ((,continuation-variable
+             (let ((,new-continuation ,continuation))
+               (lambda (&rest ,temp)
+                 (setq ,temp (car ,temp))
+                 (if (null ,temp)
+                     ,(cps client
+                           else-ast
+                           new-continuation)
+                     ,(cps client
+                           (ico:then-ast ast)
+                           new-continuation))))))
+       ,(cps client (ico:test-ast ast) continuation-variable))))
