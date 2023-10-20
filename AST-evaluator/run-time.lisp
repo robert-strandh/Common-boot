26c98abee12a4a18d6a2ec2b79ff73e018ee16d4
Robert Strandh <robert.strandh@gmail.com>
2023-10-20 15:19:09 +0200

    Make a TAGBODY-ENTRY contain a list of TAG-ENTRYs.

--- AST-evaluator/run-time.lisp
+++ AST-evaluator/run-time.lisp
@@ -17,14 +17,19 @@
     :initform t
     :accessor valid-p)))
 
-(defclass continuation-entry (dynamic-environment-entry)
+(defclass continuation-entry-mixin ()
   ((%continuation
     :initarg :continuation
-    :reader continuation)
-   ))
+    :reader continuation)))
 
-(defclass block-entry (continuation-entry valid-p-mixin)
+(defclass block-entry
+    (dynamic-environment-entry continuation-entry-mixin valid-p-mixin)
   ((%name :initarg :name :reader name)))
+
+(defmethod print-object ((object block-entry) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "name: ~s valid-p: ~s"
+            (name object) (valid-p object))))
 
 ;;; This variable is used only to pass the dynamic environment over a
 ;;; function call.  The caller assigns the current dynamic environment
@@ -53,15 +58,72 @@
         finally (error "No valid ~a entry for ~s" entry-name name)))
 
 (defun do-return-from (name dynamic-environment)
-  (unwind name dynamic-environment 'block-entry "block"))
+  (loop for rest on dynamic-environment
+        for entry = (first rest)
+        do (when (and (typep entry 'block-entry)
+                      (eq name (name entry)))
+             (if (valid-p entry)
+                 ;; FIXME: handle UNWIND-PROTECT.
+                 (progn 
+                   (loop for entry-to-invalidate in dynamic-environment
+                         until (eq entry-to-invalidate entry)
+                         do (setf (valid-p entry-to-invalidate) nil))
+                   ;; for RETURN-FROM, we also want to invalidate the
+                   ;; entry representing the associated BLOCK.
+                   (setf (valid-p entry) nil)
+                   (setf *continuation* (continuation entry))
+                   (setf *stack* (stack entry))
+                   (return))
+                 ;; For now, signal a host error.  It would be better
+                 ;; to call the target function ERROR.
+                 (error "Attempt to use an expired entry ~s~%"
+                        entry)))
+        finally (error "No valid BLOCK entry for ~s" name)))
 
-(defclass tag-entry (continuation-entry valid-p-mixin)
+(defclass tag-entry (continuation-entry-mixin)
   ((%name :initarg :name :reader name)))
 
-(defun do-go (name dynamic-environment)
-  (unwind name dynamic-environment 'tag-entry "tag"))
+(defmethod print-object ((object tag-entry) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "name: ~s" (name object))))
 
-(defclass catch-entry (continuation-entry valid-p-mixin)
+(defclass tagbody-entry (dynamic-environment-entry valid-p-mixin)
+  ((%tag-entries :initarg :tag-entries :initform '() :reader tag-entries)))
+
+(defmethod print-object ((object tagbody-entry) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "valid-p: ~s tag-entries ~s"
+            (valid-p object) (tag-entries object))))
+
+(defun do-go (name dynamic-environment)
+  (loop for rest on dynamic-environment
+        for entry = (first rest)
+        do (when (and (typep entry 'tagbody-entry)
+                      (member-if (lambda (e) (eq (name e) name))
+                                 (tag-entries entry)))
+             (if (valid-p entry)
+                 ;; FIXME: handle UNWIND-PROTECT.
+                 (progn 
+                   (loop for entry-to-invalidate in dynamic-environment
+                         until (eq entry-to-invalidate entry)
+                         do (setf (valid-p entry-to-invalidate) nil))
+                   ;; Contrary to the BLOCK entry, we do not want to
+                   ;; invalidate the TAGBODY-ENTRY, as it will be used
+                   ;; several times.  It is invalidated after the last
+                   ;; form has been executed.
+                   (let ((tag-entry (find-if (lambda (e) (eq (name e) name))
+                                             (tag-entries entry))))
+                     (setf *continuation* (continuation tag-entry)))
+                   (setf *stack* (stack entry))
+                   (return))
+                 ;; For now, signal a host error.  It would be better
+                 ;; to call the target function ERROR.
+                 (error "Attempt to use an expired entry ~s~%"
+                        entry)))
+        finally (error "No valid TAG entry for ~s" name)))
+
+(defclass catch-entry
+    (dynamic-environment-entry continuation-entry-mixin valid-p-mixin)
   ((%name :initarg :name :reader name)))
 
 (defun do-throw (name dynamic-environment)
