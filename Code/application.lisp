2268685cfea3e61152ae75a65b0cffcd703a570a
Robert Strandh <robert.strandh@gmail.com>
2023-10-23 17:39:10 +0200

    Handle applications with a lambda expression as the operator.

--- Code/application.lisp
+++ Code/application.lisp
@@ -1,9 +1,15 @@
 (cl:in-package #:common-boot)
 
 (defmethod abp:finish-node (builder kind (ast ico:application-ast))
-  (with-builder-components (builder client environment)
-    (finalize-function-name-ast-from-environment
-     client (ico:function-name-ast ast) environment))
+  (let ((operator-ast (ico:function-name-ast ast)))
+    (if (typep operator-ast 'ico:lambda-expression-ast)
+        (reinitialize-instance operator-ast
+          :form-asts
+          (loop for body-ast in (ico:form-asts operator-ast)
+                collect (convert-ast builder body-ast)))
+        (with-builder-components (builder client environment)
+          (finalize-function-name-ast-from-environment
+           client operator-ast environment))))
   ;; We assume that each argument is an unparsed form.
   (reinitialize-instance ast
     :argument-asts
