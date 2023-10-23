b6dccac5470db085568467c0cb6b007d311fc7ba
Robert Strandh <robert.strandh@gmail.com>
2023-10-23 17:57:10 +0200

    Convert forms of lambda in environment augmented by lambda list.

--- Code/application.lisp
+++ Code/application.lisp
@@ -2,12 +2,16 @@
 
 (defmethod abp:finish-node (builder kind (ast ico:application-ast))
   (let ((operator-ast (ico:function-name-ast ast)))
-    (if (typep operator-ast 'ico:lambda-expression-ast)
-        (reinitialize-instance operator-ast
-          :form-asts
-          (loop for body-ast in (ico:form-asts operator-ast)
-                collect (convert-ast builder body-ast)))
-        (with-builder-components (builder client environment)
+    (with-builder-components (builder client environment)
+      (if (typep operator-ast 'ico:lambda-expression-ast)
+          (let* ((new-environment
+                   (finalize-lambda-list
+                    client environment (ico:lambda-list-ast operator-ast)))
+                 (new-builder (make-builder client new-environment)))
+            (reinitialize-instance operator-ast
+              :form-asts
+              (loop for body-ast in (ico:form-asts operator-ast)
+                    collect (convert-ast new-builder body-ast))))
           (finalize-function-name-ast-from-environment
            client operator-ast environment))))
   ;; We assume that each argument is an unparsed form.
