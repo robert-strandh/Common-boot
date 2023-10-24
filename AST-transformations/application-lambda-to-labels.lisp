b84368106a8555f3cb420cac003dad67949bad3b
Robert Strandh <robert.strandh@gmail.com>
2023-10-24 07:45:33 +0200

    Fix references to incorrect AST.

--- AST-transformations/application-lambda-to-labels.lisp
+++ AST-transformations/application-lambda-to-labels.lisp
@@ -16,31 +16,32 @@
     ((client application-lambda-to-labels-client) (ast ico:application-ast))
   ;; Start by converting any children of this AST node.
   (call-next-method)
-  (if (typep (ico:function-name-ast ast) 'ico:lambda-expression-ast)
-      (let* ((function-name-definition-ast
-               (make-instance 'ico:local-function-name-definition-ast
-                 :name (gensym)))
-             (function-name-reference-ast
-               (make-instance 'ico:function-reference-ast
-                 :local-function-name-definition-ast
-                 function-name-definition-ast)))
-        (reinitialize-instance function-name-definition-ast
-          :local-function-name-reference-asts
-          (list function-name-reference-ast))
-        (make-instance 'ico:labels-ast
-          :binding-asts
-          (list (make-instance 'ico:local-function-ast
-                  :name-ast function-name-definition-ast
-                  :lambda-list-ast (ico:lambda-list-ast ast)
-                  :declaration-asts (ico:declaration-asts ast)
-                  :form-asts (ico:form-asts ast)
-                  :origin (ico:origin ast)))
-          :form-asts
-          (list (make-instance 'ico:application-ast
-                  :function-name-ast function-name-reference-ast
-                  :argument-asts (ico:argument-asts ast)
-                  :origin (ico:origin ast)))))
-      ast))
+  (let ((operator-ast (ico:function-name-ast ast)))
+    (if (typep operator-ast 'ico:lambda-expression-ast)
+        (let* ((function-name-definition-ast
+                 (make-instance 'ico:local-function-name-definition-ast
+                   :name (gensym)))
+               (function-name-reference-ast
+                 (make-instance 'ico:function-reference-ast
+                   :local-function-name-definition-ast
+                   function-name-definition-ast)))
+          (reinitialize-instance function-name-definition-ast
+            :local-function-name-reference-asts
+            (list function-name-reference-ast))
+          (make-instance 'ico:labels-ast
+            :binding-asts
+            (list (make-instance 'ico:local-function-ast
+                    :name-ast function-name-definition-ast
+                    :lambda-list-ast (ico:lambda-list-ast operator-ast)
+                    :declaration-asts (ico:declaration-asts operator-ast)
+                    :form-asts (ico:form-asts operator-ast)
+                    :origin (ico:origin operator-ast)))
+            :form-asts
+            (list (make-instance 'ico:application-ast
+                    :function-name-ast function-name-reference-ast
+                    :argument-asts (ico:argument-asts ast)
+                    :origin (ico:origin ast)))))
+        ast)))
                   
 (defun application-lambda-to-labels (ast)
   (let ((client (make-instance 'application-lambda-to-labels-client)))
