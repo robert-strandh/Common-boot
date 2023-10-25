7404012f849f4f184cfcf57ee1d8291031a1ffe2
Robert Strandh <robert.strandh@gmail.com>
2023-10-25 15:59:48 +0200

    Add a second test for SYMBOL-MACROLET.

--- Test/symbol-macrolet.lisp
+++ Test/symbol-macrolet.lisp
@@ -7,3 +7,11 @@
   (with-default-parameters (client environment global-environment)
     (iss #1=(symbol-macrolet ((x (+ y 10))) (let ((y 20)) x))
          (eval-expression client '#1# environment))))
+
+(define-test symbol-macrolet-two-expansions
+  :parent symbol-macrolet
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(symbol-macrolet ((x (+ y 10))
+                              (z (+ x 5)))
+              (let ((y 20)) z))
+         (eval-expression client '#1# environment))))
