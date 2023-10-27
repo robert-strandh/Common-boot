f06e0584ab44dfae2d3c09f911b8d7ba9ef43936
Robert Strandh <robert.strandh@gmail.com>
2023-10-27 17:18:28 +0200

    Fix incorrect test and add new test for one symbol one value.

--- Test/progv.lisp
+++ Test/progv.lisp
@@ -7,5 +7,13 @@
   (with-default-parameters (client environment global-environment)
     (iss #1=(let ((x 10))
               (declare (special x))
-              (progv '() (values) x))
+              (progv '() '() x))
+         (eval-expression client '#1# environment))))
+
+(define-test progv-one-symbol-one-value
+  :parent progv
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(let ((x 10))
+              (declare (special x))
+              (progv '(x) '(234)) x)
          (eval-expression client '#1# environment))))
