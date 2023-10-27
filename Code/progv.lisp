8eb6674abeb67cb20d3e81e30f7dbefcefeec135
Robert Strandh <robert.strandh@gmail.com>
2023-10-27 17:11:24 +0200

    Fix incorrect initarg.

--- Code/progv.lisp
+++ Code/progv.lisp
@@ -7,5 +7,5 @@
   (reinitialize-instance ast
     :symbols-ast (convert-ast builder (ico:symbols-ast ast))
     :values-ast (convert-ast builder (ico:values-ast ast))
-    :form-ast (loop for form-ast in (ico:form-asts ast)
-                    collect (convert-ast builder form-ast))))
+    :form-asts (loop for form-ast in (ico:form-asts ast)
+                     collect (convert-ast builder form-ast))))
