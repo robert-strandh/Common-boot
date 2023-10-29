cdd330ccb1e25b6697114d18ab5e641913c9a086
Robert Strandh <robert.strandh@gmail.com>
2023-10-29 15:04:08 +0100

    Define method specialized to LEXIFY client and LABELS-AST.

--- AST-transformations/lexify-lambda-list.lisp
+++ AST-transformations/lexify-lambda-list.lisp
@@ -140,6 +140,14 @@
   (loop for local-function-ast in (ico:binding-asts ast)
         do (lexify-lambda-list-of-ast local-function-ast))
   ast)
+
+(defmethod cbaw:walk-ast-node :around
+    ((client lexify-lambda-list-client) (ast ico:labels-ast))
+  ;; Start by converting any children of this AST node.
+  (call-next-method)
+  (loop for local-function-ast in (ico:binding-asts ast)
+        do (lexify-lambda-list-of-ast local-function-ast))
+  ast)
                   
 (defun lexify-lambda-list (ast)
   (let ((client (make-instance 'lexify-lambda-list-client)))
