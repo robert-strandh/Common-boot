4ad868aa329ada9a80ee8964dcd28858763065ea
Robert Strandh <robert.strandh@gmail.com>
2023-10-31 12:46:06 +0100

    Add test with one lexical optional parameter.

--- AST-transformations/Test/lexify-lambda-list.lisp
+++ AST-transformations/Test/lexify-lambda-list.lisp
@@ -13,3 +13,9 @@
   (is #'equal
       (parse-lexify-and-unparse '((lambda (x) 234)))
       '((lambda (x) 234))))
+
+(define-test lexify-lambda-list-already-lexified-one-optional-parameter
+  :parent lexify-lambda-list
+  (is #'equal
+      (parse-lexify-and-unparse '((lambda (&optional (x nil x-p)) 234)))
+      '((lambda (&optional (x nil x-p)) 234))))
