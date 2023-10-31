af28594bc835b06e8b224be9e8f78dd0ba76fcb2
Robert Strandh <robert.strandh@gmail.com>
2023-10-31 12:42:05 +0100

    Add test with one lexical required parameter.

--- AST-transformations/Test/lexify-lambda-list.lisp
+++ AST-transformations/Test/lexify-lambda-list.lisp
@@ -7,3 +7,9 @@
   (is #'equal
       (parse-lexify-and-unparse '((lambda () 234)))
       '((lambda () 234))))
+
+(define-test lexify-lambda-list-already-lexified-one-required-parameter
+  :parent lexify-lambda-list
+  (is #'equal
+      (parse-lexify-and-unparse '((lambda (x) 234)))
+      '((lambda (x) 234))))
