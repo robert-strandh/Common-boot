462c2232d602f973cf3916929b21aad7a9a2f2cb
Robert Strandh <robert.strandh@gmail.com>
2023-11-06 18:05:28 +0100

    Remove system for AST transformations, now in Iconoclast.

--- AST-transformations/client.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package #:common-boot-ast-transformations)
-
-;;; This class should be a superclass of any class of which an
-;;; instance is passed as the CLIENT argument to the transformations
-;;; in this system.
-
-(defclass client () ())
-               
