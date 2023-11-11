3cba990521ee54d4c63a331a08061c9835dabb49
Robert Strandh <robert.strandh@gmail.com>
2023-11-11 10:33:19 +0100

    Call FUNCTION-LAMBDA-TO-LABELS when appropriate.

--- Code/function.lisp
+++ Code/function.lisp
@@ -4,17 +4,8 @@
   (let ((name-ast (ico:name-ast ast)))
     (with-builder-components (builder client environment)
       (if (typep name-ast 'ico:lambda-expression-ast)
-          (let ((body-environment
-                  (finalize-lambda-list
-                   client environment
-                   (ico:lambda-list-ast name-ast)
-                   (ico:declaration-asts name-ast))))
-            (reinitialize-instance name-ast
-              :form-asts
-              (loop for form-ast in (ico:form-asts name-ast)
-                    collect
-                    (convert-ast-in-environment
-                     client form-ast body-environment))))
-          (finalize-function-name-ast-from-environment
-           client (ico:name-ast ast) environment))))
-  ast)
+          (let ((labels-ast (iat:function-lambda-to-labels ast)))
+            (finish-labels-ast builder labels-ast))
+          (progn (finalize-function-name-ast-from-environment
+                  client (ico:name-ast ast) environment)
+                 ast)))))
