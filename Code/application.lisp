4cbed193b8794cc912f89b8a879fb27f86a0fa72
Robert Strandh <robert.strandh@gmail.com>
2023-11-11 14:31:33 +0100

    No longer check the case where the operator is a lambda expression.

--- Code/application.lisp
+++ Code/application.lisp
@@ -3,13 +3,14 @@
 (defmethod abp:finish-node (builder kind (ast ico:application-ast))
   (let ((operator-ast (ico:function-name-ast ast)))
     (with-builder-components (builder client environment)
-      (if (typep operator-ast 'ico:lambda-expression-ast)
-          (let ((labels-ast (iat:application-lambda-to-labels ast)))
-            (finish-labels-ast builder labels-ast))
-          (progn (finalize-function-name-ast-from-environment
-                  client operator-ast environment)
-                 ;; We assume that each argument is an unparsed form.
-                 (reinitialize-instance ast
-                   :argument-asts
-                   (loop for argument-ast in (ico:argument-asts ast)
-                         collect (convert-ast builder argument-ast))))))))
+      ;; We have already turned applications of the form
+      ;; ((LAMBDA (...) ...) ...) into LABELS, so this case 
+      ;; will not happen here.
+      (finalize-function-name-ast-from-environment
+       client operator-ast environment)
+      ;; We assume that each argument is an unparsed form.
+      (reinitialize-instance ast
+        :argument-asts
+        (loop for argument-ast in (ico:argument-asts ast)
+              collect (convert-ast builder argument-ast))))))
+    
