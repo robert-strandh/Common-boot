0b7258a66e3db9e45e4523046a7f1d4444005f8a
Robert Strandh <robert.strandh@gmail.com>
2023-11-17 17:18:54 +0100

    New functions for extracting VARIABLE-ASTs from a LAMBDA-LIST-AST.

--- Code/lambda-list.lisp
+++ Code/lambda-list.lisp
@@ -116,6 +116,79 @@
                     client parameter-ast new-environment declaration-asts)))
     new-environment))
 
+(defgeneric extract-variable-asts-in-parameter (parameter-ast))
+
+(defmethod extract-variable-asts-in-parameter
+    ((parameter-ast ico:required-parameter-ast))
+  (list (ico:name-ast parameter-ast)))
+
+(defmethod extract-variable-asts-in-parameter
+    ((parameter-ast ico:optional-parameter-ast))
+  (let ((name-ast (ico:name-ast parameter-ast))
+        (supplied-p-ast (ico:supplied-p-parameter-ast parameter-ast)))
+    (if (null supplied-p-ast)
+        (list name-ast)
+        (list name-ast supplied-p-ast))))
+
+(defmethod extract-variable-asts-in-parameter
+    ((parameter-ast ico:rest-parameter-ast))
+  (list (ico:name-ast parameter-ast)))
+
+(defmethod extract-variable-asts-in-parameter
+    ((parameter-ast ico:key-parameter-ast))
+  (let ((name-ast (ico:name-ast parameter-ast))
+        (supplied-p-ast (ico:supplied-p-parameter-ast parameter-ast)))
+    (if (null supplied-p-ast)
+        (list name-ast)
+        (list name-ast supplied-p-ast))))
+
+(defmethod extract-variable-asts-in-parameter
+    ((parameter-ast ico:aux-parameter-ast))
+  (list (ico:name-ast parameter-ast)))
+
+(defgeneric extract-variable-asts-in-section (section-ast))
+
+(defmethod extract-variable-asts-in-section
+    ((section-ast null))
+  '())
+
+(defmethod extract-variable-asts-in-section
+    ((section-ast ico:single-parameter-section-ast))
+  (extract-variable-asts-in-parameter (ico:parameter-ast section-ast)))
+
+(defmethod extract-variable-asts-in-section
+    ((section-ast ico:multi-parameter-section-ast))
+  (loop for parameter-ast in (ico:parameter-asts section-ast)
+        append (extract-variable-asts-in-parameter parameter-ast)))
+
+(defgeneric extract-variable-asts-in-lambda-list (lambda-list-ast))
+
+(defmethod extract-variable-asts-in-lambda-list
+    ((ast ico:ordinary-lambda-list-ast))
+  (append (extract-variable-asts-in-section
+           (ico:required-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:optional-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:rest-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:key-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:aux-section-ast ast))))
+
+(defmethod extract-variable-asts-in-lambda-list
+    ((ast ico:specialized-lambda-list-ast))
+  (append (extract-variable-asts-in-section
+           (ico:required-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:optional-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:rest-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:key-section-ast ast))
+          (extract-variable-asts-in-section
+           (ico:aux-section-ast ast))))
+
 ;;; FIXME: handle declarations. 
 
 (defgeneric finalize-lambda-list
