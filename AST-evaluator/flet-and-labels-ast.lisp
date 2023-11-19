2e286c57e6a97e0ceed25502cfb37102840b60a9
Robert Strandh <robert.strandh@gmail.com>
2023-11-19 16:46:54 +0100

    Enter lambda-list variables into the environment.

--- AST-evaluator/flet-and-labels-ast.lisp
+++ AST-evaluator/flet-and-labels-ast.lisp
@@ -2,6 +2,11 @@
 
 (defun cps-function-ast
     (client lambda-list-ast form-asts continuation)
+  (let ((lambda-list-variable-asts
+          (common-boot::extract-variable-asts-in-lambda-list
+           lambda-list-ast)))
+    (loop for lambda-list-variable-ast in lambda-list-variable-asts
+          do (setf (lookup lambda-list-variable-ast) (gensym))))
   (let* ((let*-ast (make-instance 'ico:let*-ast))
          (variable-name (make-symbol "ARGUMENTS"))
          (variable-ast
