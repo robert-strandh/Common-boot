b7b85c938237d35b65ad6385227adc6537476c1e
Robert Strandh <robert.strandh@gmail.com>
2023-11-20 15:55:55 +0100

    Remove EXTRACT-VARIABLE-ASTS-IN-LAMBDA-LIST now in Iconoclast.

--- Code/lambda-list.lisp
+++ Code/lambda-list.lisp
@@ -92,75 +92,6 @@
                     client parameter-ast new-environment)))
     new-environment))
 
-(defgeneric extract-variable-asts-in-parameter (parameter-ast))
-
-(defmethod extract-variable-asts-in-parameter
-    ((parameter-ast ico:required-parameter-ast))
-  (list (ico:name-ast parameter-ast)))
-
-(defmethod extract-variable-asts-in-parameter
-    ((parameter-ast ico:optional-parameter-ast))
-  (let ((name-ast (ico:name-ast parameter-ast))
-        (supplied-p-ast (ico:supplied-p-parameter-ast parameter-ast)))
-    (if (null supplied-p-ast)
-        (list name-ast)
-        (list name-ast supplied-p-ast))))
-
-(defmethod extract-variable-asts-in-parameter
-    ((parameter-ast ico:rest-parameter-ast))
-  (list (ico:name-ast parameter-ast)))
-
-(defmethod extract-variable-asts-in-parameter
-    ((parameter-ast ico:key-parameter-ast))
-  (let ((name-ast (ico:name-ast parameter-ast))
-        (supplied-p-ast (ico:supplied-p-parameter-ast parameter-ast)))
-    (if (null supplied-p-ast)
-        (list name-ast)
-        (list name-ast supplied-p-ast))))
-
-(defmethod extract-variable-asts-in-parameter
-    ((parameter-ast ico:aux-parameter-ast))
-  (list (ico:name-ast parameter-ast)))
-
-(defgeneric extract-variable-asts-in-section (section-ast))
-
-(defmethod extract-variable-asts-in-section
-    ((section-ast null))
-  '())
-
-(defmethod extract-variable-asts-in-section
-    ((section-ast ico:single-parameter-section-ast))
-  (extract-variable-asts-in-parameter (ico:parameter-ast section-ast)))
-
-(defmethod extract-variable-asts-in-section
-    ((section-ast ico:multi-parameter-section-ast))
-  (loop for parameter-ast in (ico:parameter-asts section-ast)
-        append (extract-variable-asts-in-parameter parameter-ast)))
-
-(defgeneric extract-variable-asts-in-lambda-list (lambda-list-ast))
-
-(defmethod extract-variable-asts-in-lambda-list
-    ((ast ico:ordinary-lambda-list-ast))
-  (loop for accessor
-          in (list #'ico:required-section-ast
-                   #'ico:optional-section-ast
-                   #'ico:rest-section-ast
-                   #'ico:key-section-ast
-                   #'ico:aux-section-ast)
-        append (extract-variable-asts-in-section
-                (funcall accessor ast))))
-
-(defmethod extract-variable-asts-in-lambda-list
-    ((ast ico:specialized-lambda-list-ast))
-  (loop for accessor
-          in (list #'ico:required-section-ast
-                   #'ico:optional-section-ast
-                   #'ico:rest-section-ast
-                   #'ico:key-section-ast
-                   #'ico:aux-section-ast)
-        append (extract-variable-asts-in-section
-                (funcall accessor ast))))
-
 ;;; We account for the possibility of the lambda list to contain
 ;;; duplicate variables, just like a LET* can.  So the variable
 ;;; referred to by some SPECIAL declaration is the rightmost one with
@@ -168,7 +99,7 @@
 ;;; variables in the lambda list in reverse order.
 (defun mark-variable-ast-as-special (lambda-list-ast variable-name-ast)
   (let* ((variable-asts
-           (extract-variable-asts-in-lambda-list lambda-list-ast)))
+           (iat:extract-variable-asts-in-lambda-list lambda-list-ast)))
     (loop for variable-ast in (reverse variable-asts)
           do (when (eq (ico:name variable-ast)
                        (ico:name variable-name-ast))
