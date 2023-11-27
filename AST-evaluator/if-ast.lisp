930074974ad949d1e63cc12a3c4006373a953c4d
Robert Strandh <robert.strandh@gmail.com>
2023-11-27 05:39:39 +0100

    Improve CPS method for IF-AST.

--- AST-evaluator/if-ast.lisp
+++ AST-evaluator/if-ast.lisp
@@ -1,22 +1,37 @@
 (cl:in-package #:common-boot-ast-evaluator)
 
 (defmethod cps (client environment (ast ico:if-ast) continuation)
-  (let ((temp (gensym "TEST"))
-        (continuation-variable (gensym "C-"))
+  (let ((temp (gensym))
+        (test-continuation (gensym))
+        (then-continuation (gensym))
+        (else-continuation (gensym))
+        (temp-continuation (gensym))
+        (test-ast (ico:test-ast ast))
+        (then-ast (ico:then-ast ast))
         (else-ast (if (null (ico:else-ast ast))
                       (make-instance 'ico:literal-ast :literal nil)
                       (ico:else-ast ast))))
-    `(let ((,continuation-variable
-             (make-continuation
-              (lambda (&rest ,temp)
-                (setq ,temp (car ,temp))
-                (if (null ,temp)
-                    ,(cps client environment
-                          else-ast
-                          continuation)
-                    ,(cps client environment
-                          (ico:then-ast ast)
-                          continuation)))
-              :origin ',(ico:origin ast)
-              :next ,continuation)))
-       ,(cps client environment (ico:test-ast ast) continuation-variable))))
+    `(let* ((,then-continuation
+              (make-continuation
+               (lambda ()
+                 ,(cps client environment then-ast continuation))
+               :origin ',(ico:origin then-ast)
+               :next ,continuation))
+            (,else-continuation
+              (make-continuation
+               (lambda ()
+                 ,(cps client environment else-ast continuation))
+               :origin ',(ico:origin else-ast)
+               :next ,continuation))
+            (,temp-continuation
+              (make-continuation
+               (lambda (&rest ,temp)
+                 (setq ,temp (car ,temp))
+                 (if (null ,temp)
+                     (step '() ,else-continuation)
+                     (step '() ,then-continuation)))))
+            (,test-continuation
+              (make-continuation
+               (lambda ()
+                 ,(cps client environment test-ast temp-continuation)))))
+       (step '() ,test-continuation))))
