126dea5af9566f9ac4d0cf20d839071f9addc4d0
Robert Strandh <robert.strandh@gmail.com>
2023-11-27 13:42:53 +0100

    Introduce AFTER-CONTINUATIONs.

--- AST-evaluator/if-ast.lisp
+++ AST-evaluator/if-ast.lisp
@@ -2,10 +2,10 @@
 
 (defmethod cps (client environment (ast ico:if-ast) continuation)
   (let ((temp (gensym))
-        (test-continuation (gensym))
+        (test-before-continuation (gensym))
+        (test-after-continuation (gensym))
         (then-continuation (gensym))
         (else-continuation (gensym))
-        (temp-continuation (gensym))
         (test-ast (ico:test-ast ast))
         (then-ast (ico:then-ast ast))
         (else-ast (if (null (ico:else-ast ast))
@@ -23,17 +23,24 @@
                  ,(cps client environment else-ast continuation))
                :origin ',(ico:origin else-ast)
                :next ,continuation))
-            (,temp-continuation
-              (make-before-continuation
+            (,test-after-continuation
+              (make-after-continuation
                (lambda (&rest ,temp)
+                 (setf (results *continuation*) ,temp)
                  (setq ,temp (car ,temp))
                  (if (null ,temp)
-                     (step '() ,else-continuation)
-                     (step '() ,then-continuation)))))
-            (,test-continuation
+                     (progn (setf (next-continuation *continuation*)
+                                  ,else-continuation)
+                            (step '() ,else-continuation))
+                     (progn (setf (next-continuation *continuation*)
+                                  ,then-continuation)
+                            (step '() ,then-continuation))))
+               :origin ',(ico:origin test-ast)
+               :next ,continuation))
+            (,test-before-continuation
               (make-before-continuation
                (lambda ()
-                 ,(cps client environment test-ast temp-continuation))
+                 ,(cps client environment test-ast test-after-continuation))
                :origin ',(ico:origin test-ast)
-               :next ,temp-continuation)))
-       (step '() ,test-continuation))))
+               :next ,test-after-continuation)))
+       (step '() ,test-before-continuation))))
