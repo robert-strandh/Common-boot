ab597fd5f1ec9e17cc9549a2d71ace263ae0f145
Robert Strandh <robert.strandh@gmail.com>
2023-11-29 09:22:39 +0100

    Add test for LET with special variable binding.

--- Test/let.lisp
+++ Test/let.lisp
@@ -25,3 +25,9 @@
   (with-default-parameters (client environment global-environment)
     (iss #1=(let ((x 234)) (let ((x 345) (y x)) (+ x y)))
          (eval-expression client '#1# environment))))
+
+(define-test let-with-special-variable
+  :parent let
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(let ((x 234)) (declare (special x)) x)
+         (eval-expression client '#1# environment))))
