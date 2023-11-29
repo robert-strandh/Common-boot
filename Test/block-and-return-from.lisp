0df69e975ed1c903cbf651095b55163f8f7016b6
Robert Strandh <robert.strandh@gmail.com>
2023-11-29 16:57:56 +0100

    Add test case with RETURN-FROM as last form in BLOCK.

--- Test/block-and-return-from.lisp
+++ Test/block-and-return-from.lisp
@@ -14,6 +14,12 @@
     (iss #1=(block b (return-from b 345) 234)
          (eval-expression client '#1# environment))))
 
+(define-test block-with-return-from-as-last-form
+  :parent block-and-return-from
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(block b 234 (return-from b 345))
+         (eval-expression client '#1# environment))))
+
 (define-test block-with-return-from-inside-let
   :parent block-and-return-from
   (with-default-parameters (client environment global-environment)
