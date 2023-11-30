9840f6c7877c0b81f12c73fa5aed3f74872419e4
Robert Strandh <robert.strandh@gmail.com>
2023-11-30 15:24:57 +0100

    Add test case with two BLOCKs in a row.

--- Test/block-and-return-from.lisp
+++ Test/block-and-return-from.lisp
@@ -25,3 +25,12 @@
   (with-default-parameters (client environment global-environment)
     (iss #1=(block b (let () (return-from b 345) 234))
          (eval-expression client '#1# environment))))
+
+(define-test block-two-in-a-row
+  :parent block-and-return-from
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(+ (block a
+                 (return-from a 10))
+               (block b
+                 (return-from b 20)))
+         (eval-expression client '#1# environment))))
