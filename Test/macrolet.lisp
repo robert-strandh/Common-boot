b988a64d84301f677f882f41074318b9cc62176f
Robert Strandh <robert.strandh@gmail.com>
2023-12-14 15:33:03 +0100

    Add a very simple test case for MACROLET.

--- Test/macrolet.lisp
+++ Test/macrolet.lisp
@@ -2,6 +2,12 @@
 
 (define-test macrolet)
 
+(define-test macrolet-one-very-simple-macro
+  :parent macrolet
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(macrolet ((foo () 234)) (foo))
+         (eval-expression client '#1# environment))))
+
 (define-test macrolet-one-simple-macro
   :parent macrolet
   (with-default-parameters (client environment global-environment)
