fac7ac716c06bb1c4d68df8baf9bc33cca53e791
Robert Strandh <robert.strandh@gmail.com>
2023-12-16 06:48:07 +0100

    Issue a warning for undefined global functions.

--- Code/environment-query.lisp
+++ Code/environment-query.lisp
@@ -5,27 +5,39 @@
   (let* ((function-name (cst:raw function-name-cst))
          (result (trucler:describe-function
                   client environment function-name)))
-    (loop while (null result)
-          do (restart-case (error "No function description for ~s"
-                                  function-name)
-               (consider-global ()
-                 :report (lambda (stream)
-                           (format stream
-                                   "Treat it as the name of a global function."))
-                 (return-from describe-function
-                   (make-instance 'trucler:global-function-description
-                     ;; :origin (cst:source function-name-cst)
-                     :name function-name)))
-               (substitute (new-function-name)
-                 :report (lambda (stream)
-                           (format stream "Substitute a different name."))
-                 :interactive (lambda ()
-                                (format *query-io* "Enter new name: ")
-                                (list (read *query-io*)))
-                 (setq result
-                       (trucler:describe-function
-                        client environment new-function-name)))))
+    (when (null result)
+      (warn "No function description for ~s" function-name)
+      (return-from describe-function
+        (make-instance 'trucler:global-function-description
+          :name function-name)))
     result))
+
+;; (defun describe-function (client environment function-name-cst)
+;;   #+sbcl(declare (sb-ext:muffle-conditions sb-ext:compiler-note))
+;;   (let* ((function-name (cst:raw function-name-cst))
+;;          (result (trucler:describe-function
+;;                   client environment function-name)))
+;;     (loop while (null result)
+;;           do (restart-case (error "No function description for ~s"
+;;                                   function-name)
+;;                (consider-global ()
+;;                  :report (lambda (stream)
+;;                            (format stream
+;;                                    "Treat it as the name of a global function."))
+;;                  (return-from describe-function
+;;                    (make-instance 'trucler:global-function-description
+;;                      ;; :origin (cst:source function-name-cst)
+;;                      :name function-name)))
+;;                (substitute (new-function-name)
+;;                  :report (lambda (stream)
+;;                            (format stream "Substitute a different name."))
+;;                  :interactive (lambda ()
+;;                                 (format *query-io* "Enter new name: ")
+;;                                 (list (read *query-io*)))
+;;                  (setq result
+;;                        (trucler:describe-function
+;;                         client environment new-function-name)))))
+;;     result))
 
 (defun describe-tag (client environment tag-name)
   (let ((result (trucler:describe-tag client environment tag-name)))
