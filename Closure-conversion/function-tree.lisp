ebce3db08e3edde29fc8b0309c15756683d2763e
Robert Strandh <robert.strandh@gmail.com>
2024-06-26 16:37:25 +0200

    Fix incorrect code.

--- Closure-conversion/function-tree.lisp
+++ Closure-conversion/function-tree.lisp
@@ -7,24 +7,25 @@
    (%parent :initarg :parent :reader parent)
    (%children :initform '() :accessor children)))
 
-;;; This variable holds the current FUNCTION-AST that we are
+;;; This variable holds the current FUNCTION-NODE that we are
 ;;; traversing.  When a new FUNCTION-AST is encountered, this variable
-;;; holds the FUNCTION-AST that will be the parent of the new one, and
-;;; then this variable will be bound to the new FUNCTION-AST.
-(defvar *current-function-ast*)
+;;; holds the FUNCTION-NODE that will be the parent of the new one, and
+;;; then this variable will be bound to the new FUNCTION-NODE.
+(defvar *current-function-node*)
 
-(defmethod iaw:walk-ast :around ((client client) (ast ico:function-ast))
-  (push ast (children *current-function-ast*))
-  (let ((*current-function-ast*
-          (make-instance 'function-node
-            :function-ast ast
-            :parent *current-function-ast*)))
-    (call-next-method)))
+(defmethod iaw:walk-ast-node :around ((client client) (ast ico:function-ast))
+  (let ((new-node (make-instance 'function-node
+                    :function-ast ast
+                    :parent *current-function-node*)))
+    (push new-node (children *current-function-node*))
+    (let ((*current-function-node* new-node))
+      (call-next-method))))
 
 (defun create-function-tree (ast)
-  (let ((root (make-instance 'function-node
-                :function-ast nil
-                :parent nil))
-        (client (make-instance 'client)))
+  (let* ((*current-function-node*
+           (make-instance 'function-node
+             :function-ast nil
+             :parent nil))
+         (client (make-instance 'client)))
     (iaw:walk-ast client ast)
-    root))
+    *current-function-node*))
