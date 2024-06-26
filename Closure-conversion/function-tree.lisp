abfaad8b8ec31d4a0d728492c5aac57d1efb6627
Robert Strandh <robert.strandh@gmail.com>
2024-06-26 15:15:25 +0200

    Define :AROUND method on WALK-AST specialized to FUNCTION-AST.

--- Closure-conversion/function-tree.lisp
+++ Closure-conversion/function-tree.lisp
@@ -6,3 +6,17 @@
   ((%function-ast :initarg :function-ast :reader function-ast)
    (%parent :initarg :parent :reader parent)
    (%children :initform '() :accessor children)))
+
+;;; This variable holds the current FUNCTION-AST that we are
+;;; traversing.  When a new FUNCTION-AST is encountered, this variable
+;;; holds the FUNCTION-AST that will be the parent of the new one, and
+;;; then this variable will be bound to the new FUNCTION-AST.
+(defvar *current-function-ast*)
+
+(defmethod iaw:walk-ast :around ((client client) (ast ico:function-ast))
+  (push ast (children *current-function-ast*))
+  (let ((*current-function-ast*
+          (make-instance 'function-node
+            :function-ast ast
+            :parent *current-function-ast*)))
+    (call-next-method)))
