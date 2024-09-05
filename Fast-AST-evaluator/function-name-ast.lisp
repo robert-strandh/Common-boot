9f2434a3740f17cdbabbe15b25f4d5053224ea9a
Robert Strandh <robert.strandh@gmail.com>
2024-09-05 15:53:03 +0200

    Add method for GLOBAL-FUNCTION-NAME-REFERENCE-AST

--- Fast-AST-evaluator/function-name-ast.lisp
+++ Fast-AST-evaluator/function-name-ast.lisp
@@ -4,3 +4,8 @@
 (defmethod translate-ast
     (client environment (ast ico:function-reference-ast))
   `(function ,(ico:name ast)))
+
+(defmethod translate-ast
+    (client environment (ast ico:global-function-name-reference-ast))
+  (let ((name (ico:name (ico:name-ast ast))))
+    `(car ,(clostrum:ensure-operator-cell client environment name))))
