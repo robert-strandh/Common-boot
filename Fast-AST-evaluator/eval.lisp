e92dc70f400061b63993b18459c60c07700c1f66
Robert Strandh <robert.strandh@gmail.com>
2024-09-05 10:31:48 +0200

    Define COMPILE-AST, TRANSLATE, and TRANSLATE-AST.

--- Fast-AST-evaluator/eval.lisp
+++ Fast-AST-evaluator/eval.lisp
@@ -1,8 +1,16 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
+(defgeneric translate-ast (client ast environment))
+
+(defun translate (client ast environment)
+  (translate-ast client ast environment))
+
+(defun compile-ast (client ast environment)
+  (compile nil `(lambda () ,(translate client ast environment))))
+
 (defmethod eval-cst (client cst environment)
   (let* ((ast (cb:cst-to-ast client cst environment))
-         (builder (make-builder client environment))
+         (builder (cb::make-builder client environment))
          (top-level-function 
            (cm:with-builder builder
              (compile-ast client ast environment))))
