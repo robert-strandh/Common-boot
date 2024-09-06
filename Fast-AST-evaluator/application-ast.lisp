275628e6ab12a9906f0e14a5441f978ddb2d4950
Robert Strandh <robert.strandh@gmail.com>
2024-09-06 17:50:34 +0200

    Bind special *DYNAMIC-ENVIRONMENT* from lexical DYNAMIC-ENVIRONMENT.

--- Fast-AST-evaluator/application-ast.lisp
+++ Fast-AST-evaluator/application-ast.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
 (defmethod translate-ast (client environment (ast ico:application-ast))
-  `(funcall
-    ,(translate-ast client environment (ico:function-name-ast ast))
-    ,@(loop for argument-ast in (ico:argument-asts ast)
-            collect (translate-ast environment client argument-ast))))
+  `(let ((*dynamic-environment* dynamic-environment))
+     (funcall
+      ,(translate-ast client environment (ico:function-name-ast ast))
+      ,@(loop for argument-ast in (ico:argument-asts ast)
+              collect (translate-ast environment client argument-ast)))))
