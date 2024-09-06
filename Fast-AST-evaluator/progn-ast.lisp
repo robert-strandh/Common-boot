eb4710e1b02f1341db37254f446335722a65f994
Robert Strandh <robert.strandh@gmail.com>
2024-09-06 15:25:19 +0200

    Fix incorrect unquote version.

--- Fast-AST-evaluator/progn-ast.lisp
+++ Fast-AST-evaluator/progn-ast.lisp
@@ -1,9 +1,10 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
 (defun translate-implicit-progn (client environment asts)
-  `(progn ,(loop for ast in asts
-                 collect (translate-ast client environment ast))))
+  (loop for ast in asts
+        collect (translate-ast client environment ast)))
   
 
 (defmethod translate-ast (client environment (ast ico:progn-ast))
-  (translate-implicit-progn client environment (ico:form-asts ast)))
+  `(progn
+     ,@(translate-implicit-progn client environment (ico:form-asts ast))))
