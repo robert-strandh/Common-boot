cfceaccdf15c6d58a8b3fcf35c4620935cfc9e7b
Robert Strandh <robert.strandh@gmail.com>
2024-09-09 11:21:12 +0200

    Handle case where no form is present.

--- Fast-AST-evaluator/return-from-ast.lisp
+++ Fast-AST-evaluator/return-from-ast.lisp
@@ -2,9 +2,12 @@
 
 (defmethod translate-ast (client environment (ast ico:return-from-ast))
   (let* ((form-ast (ico:form-ast ast))
-         (form (translate-ast client environment form-ast))
+         (form (if (null form-ast) nil (translate-ast client environment form-ast)))
          (block-name-ast (ico:name-ast ast))
          (name (lookup (ico:block-name-definition-ast block-name-ast))))
     `(let* ((entry (do-return-from ',name dynamic-environment))
             (unwinder (unwinder entry)))
-       (funcall unwinder (multiple-value-list ,form)))))
+       (funcall unwinder
+                ,(if (null form)
+                     '()
+                     `(multiple-value-list ,form))))))
