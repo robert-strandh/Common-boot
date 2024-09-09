69c0a8fc6bf38b52e90de6c65cf59a92ef82ed7e
Robert Strandh <robert.strandh@gmail.com>
2024-09-09 17:09:19 +0200

    Take into account special variables.

--- Fast-AST-evaluator/setq-ast.lisp
+++ Fast-AST-evaluator/setq-ast.lisp
@@ -1,7 +1,23 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
+(defun create-pair (client environment variable-name-ast value-ast)
+  (if (typep variable-name-ast 'ico:special-variable-reference-ast)
+      (let ((global-environment
+              (trucler:global-environment client environment)))
+        `(setf (symbol-value
+                ',(ico:name variable-name-ast)
+                ',(clostrum-sys:variable-cell
+                   client
+                   global-environment
+                   (ico:name variable-name-ast))
+                dynamic-environment)
+               ,(translate-ast client environment value-ast)))
+      `(setq ,(translate-ast client environment variable-name-ast)
+             ,(translate-ast client environment value-ast))))
+
 (defmethod translate-ast (client environment (ast ico:setq-ast))
-  `(setq ,@(loop for variable-name-ast in (ico:variable-name-asts ast)
-                 for value-ast in (ico:value-asts ast)
-                 collect (translate-ast client environment variable-name-ast)
-                 collect (translate-ast client environment value-ast))))
+  `(progn
+     ,@(loop for variable-name-ast in (ico:variable-name-asts ast)
+             for value-ast in (ico:value-asts ast)
+             collect (create-pair client environment
+                                  variable-name-ast value-ast))))
