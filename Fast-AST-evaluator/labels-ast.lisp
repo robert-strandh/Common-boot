6ce1a0802acf59782f61572d2b5b49b38b288402
Robert Strandh <robert.strandh@gmail.com>
2024-09-11 15:28:24 +0200

    Declare the lambda-list variables IGNORABLE.

--- Fast-AST-evaluator/labels-ast.lisp
+++ Fast-AST-evaluator/labels-ast.lisp
@@ -4,13 +4,18 @@
   (setf (lookup (ico:name-ast ast)) (gensym))
   (let* ((lambda-list-ast (ico:lambda-list-ast ast))
          (lambda-list-variable-asts
-           (iat:extract-variable-asts-in-lambda-list lambda-list-ast)))
+           (iat:extract-variable-asts-in-lambda-list lambda-list-ast))
+         (lambda-list-variables
+           (loop for lambda-list-variable-ast in lambda-list-variable-asts
+                 collect (gensym))))
     (loop for lambda-list-variable-ast in lambda-list-variable-asts
-          do (setf (lookup lambda-list-variable-ast) (gensym)))
+          for lambda-list-variable in lambda-list-variables
+          do (setf (lookup lambda-list-variable-ast) lambda-list-variable))
     (let ((host-lambda-list
             (host-lambda-list-from-lambda-list-ast lambda-list-ast)))
       `(,(lookup (ico:name-ast ast))
         ,host-lambda-list
+        (declare (ignorable ,@lambda-list-variables))
         (let ((dynamic-environment *dynamic-environment*))
           (declare (ignorable dynamic-environment))
           ,@(translate-implicit-progn
