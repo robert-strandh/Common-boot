1cd27232d22fc0c41c74562d8b125354fd83d1ed
Robert Strandh <robert.strandh@gmail.com>
2024-09-11 15:28:09 +0200

    Define class STACK-ENTRY.

--- Fast-AST-evaluator/run-time.lisp
+++ Fast-AST-evaluator/run-time.lisp
@@ -190,4 +190,9 @@
              (return value))
         finally (return (setf (car cell) value))))
 
+(defclass stack-entry ()
+  ((%origin :initarg :origin :reader origin)
+   (%called-function :initarg :called-function :reader called-function)
+   (%arguments :initarg :arguments :reader arguments)))
+
 (defparameter *stack* '())
