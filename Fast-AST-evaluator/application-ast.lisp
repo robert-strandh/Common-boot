d21853494b22d378a2b80fac4a77df8bfb2ec5a8
Robert Strandh <robert.strandh@gmail.com>
2024-09-11 15:27:50 +0200

    Add a stack entry when a function is called.

--- Fast-AST-evaluator/application-ast.lisp
+++ Fast-AST-evaluator/application-ast.lisp
@@ -1,8 +1,16 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
 (defmethod translate-ast (client environment (ast ico:application-ast))
-  `(let ((*dynamic-environment* dynamic-environment))
-     (funcall
-      ,(translate-ast client environment (ico:function-name-ast ast))
-      ,@(loop for argument-ast in (ico:argument-asts ast)
-              collect (translate-ast client environment argument-ast)))))
+  `(let ((*dynamic-environment* dynamic-environment)
+         (function
+           ,(translate-ast client environment (ico:function-name-ast ast)))
+         (arguments
+           (list ,@(loop for argument-ast in (ico:argument-asts ast)
+                         collect (translate-ast
+                                  client environment argument-ast)))))
+     (let ((*stack* (cons (make-instance 'stack-entry
+                            :origin ',(ico:origin ast)
+                            :called-function function
+                            :arguments arguments)
+                          *stack*)))
+       (apply function arguments))))
