ea1970358474519e02079eddb35bee42389a9579
Robert Strandh <robert.strandh@gmail.com>
2024-09-11 16:38:26 +0200

    Remove function, now taken from COMMON-BOOT.

--- Fast-AST-evaluator/eval.lisp
+++ Fast-AST-evaluator/eval.lisp
@@ -24,21 +24,3 @@
           #+sbcl
           (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
           ,(translate client simplified-ast environment))))))
-
-(defmethod eval-cst (client cst environment)
-  (let* ((ast (cb:cst-to-ast client cst environment))
-         (builder (cb::make-builder client environment))
-         (top-level-function 
-           (cm:with-builder builder
-             (compile-ast client ast environment))))
-    (funcall top-level-function)))
-
-(defun ast-from-expression (expression environment)
-  (let* ((cst (cst:cst-from-expression expression))
-         (client (make-instance 'trucler-reference:client)))
-    (cb:cst-to-ast client cst environment)))
-
-(defun eval-expression (client expression environment)
-  (eval-cst client
-            (cst:cst-from-expression expression)
-            environment))
