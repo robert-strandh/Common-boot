eb86d0c370e075c027a03504002555bdd99f80a1
Robert Strandh <robert.strandh@gmail.com>
2024-09-12 15:23:42 +0200

    Wrap the code in LOAD-TIME-VALUE and add a comment.

--- Fast-AST-evaluator/function-name-ast.lisp
+++ Fast-AST-evaluator/function-name-ast.lisp
@@ -10,4 +10,12 @@
     (client environment (ast ico:global-function-name-reference-ast))
   (let ((name (ico:name ast))
         (env (trucler:global-environment client environment)))
-    `(car ',(clostrum:ensure-operator-cell client env name))))
+    `(car (load-time-value
+           ',(clostrum:ensure-operator-cell client env name) nil))))
+
+;;; This code used to read:
+;;; `(car ',(clostrum:ensure-operator-cell client env name))
+;;; but then at least one compiler optimizes it so that the call to
+;;; CAR is removed.  As a result, if the function of the cell is
+;;; undefined at compile time, we always get the function that signals
+;;; an UNDEFINED-FUNCTION error.
