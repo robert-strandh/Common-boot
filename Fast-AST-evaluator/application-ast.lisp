cd5ae399c14f3a98e83c7bfb5cf1e0951e38ce46
Robert Strandh <robert.strandh@gmail.com>
2024-09-13 15:46:45 +0200

    Create a stack entry only for global function calls.

--- Fast-AST-evaluator/application-ast.lisp
+++ Fast-AST-evaluator/application-ast.lisp
@@ -1,16 +1,22 @@
 (cl:in-package #:common-boot-fast-ast-evaluator)
 
 (defmethod translate-ast (client environment (ast ico:application-ast))
-  `(let ((*dynamic-environment* dynamic-environment)
-         (function
-           ,(translate-ast client environment (ico:function-name-ast ast)))
-         (arguments
-           (list ,@(loop for argument-ast in (ico:argument-asts ast)
-                         collect (translate-ast
-                                  client environment argument-ast)))))
-     (let ((*stack* (cons (make-instance 'stack-entry
-                            :origin ',(ico:origin ast)
-                            :called-function function
-                            :arguments arguments)
-                          *stack*)))
-       (apply function arguments))))
+  (let ((function-name-ast (ico:function-name-ast ast))
+        (argument-forms (loop for argument-ast in (ico:argument-asts ast)
+                              collect (translate-ast
+                                       client environment argument-ast))))
+    (if (typep function-name-ast 'ico:global-function-name-reference-ast)
+        `(let ((*dynamic-environment* dynamic-environment)
+               (function
+                 ,(translate-ast client environment function-name-ast))
+               (arguments (list ,@argument-forms)))
+           (let ((*stack* (cons (make-instance 'stack-entry
+                                  :origin ',(ico:origin ast)
+                                  :called-function function
+                                  :arguments arguments)
+                                *stack*)))
+             (apply function arguments)))
+        `(let ((*dynamic-environment* dynamic-environment)
+               (function
+                 ,(translate-ast client environment function-name-ast)))
+           (funcall function ,@argument-forms)))))
