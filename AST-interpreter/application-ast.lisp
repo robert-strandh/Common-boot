107287f165b1d7aa030122c826db86ff82e35375
Robert Strandh <robert.strandh@gmail.com>
2024-09-16 14:32:02 +0200

    Fix incorrect handling of the argument ASTs.

--- AST-interpreter/application-ast.lisp
+++ AST-interpreter/application-ast.lisp
@@ -2,5 +2,5 @@
 
 (defmethod interpret-ast (client environment (ast ico:application-ast))
   (apply (interpret-ast client environment (ico:function-name-ast ast))
-         (interpret-implicit-progn-asts
-          client environment (ico:argument-asts ast))))
+         (loop for argument-ast in (ico:argument-asts ast)
+               collect (interpret-ast client environment argument-ast))))
