0933ade65fdce0bf983b12db404ca1efdd2ae2c2
Robert Strandh <robert.strandh@gmail.com>
2024-09-16 16:28:02 +0200

    Call the unwinder.

--- AST-interpreter/return-from-ast.lisp
+++ AST-interpreter/return-from-ast.lisp
@@ -2,5 +2,11 @@
 
 (defmethod interpret-ast (client environment (ast ico:return-from-ast))
   (let* ((name-ast (ico:name-ast ast))
-         (definition-ast (ico:block-name-definition-ast name-ast)))
-    (do-return-from definition-ast *dynamic-environment*)))
+         (definition-ast (ico:block-name-definition-ast name-ast))
+         (entry (do-return-from definition-ast *dynamic-environment*)))
+    (funcall
+     (unwinder entry)
+     (if (null (ico:form-ast ast))
+         '()
+         (multiple-value-list
+          (interpret-ast client environment (ico:form-ast ast)))))))
