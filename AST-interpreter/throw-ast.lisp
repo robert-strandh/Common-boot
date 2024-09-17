0a02dd3e7852e906e1af1105831d33560d91a2a0
Robert Strandh <robert.strandh@gmail.com>
2024-09-17 14:58:06 +0200

    New component containing method for THROW-AST

---
+++ AST-interpreter/throw-ast.lisp
@@ -0,0 +1,7 @@
+(cl:in-package #:common-boot-ast-interpreter)
+
+(defmethod interpret-ast (client environment (ast ico:throw-ast))
+  (let ((entry (do-throw (ico:tag-ast ast) *dynamic-environment*)))
+    (funcall (unwinder entry)
+             (multiple-value-list
+              (intepret-ast client environment (ico:form-ast ast))))))
