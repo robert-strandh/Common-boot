960017455f635e98c46b567f4c484f807fca7bc2
Robert Strandh <robert.strandh@gmail.com>
2024-09-17 15:15:27 +0200

    Evaluate the tag.

--- AST-interpreter/catch-ast.lisp
+++ AST-interpreter/catch-ast.lisp
@@ -1,13 +1,15 @@
 (cl:in-package #:common-boot-ast-interpreter)
 
 (defmethod interpret-ast (client environment (ast ico:catch-ast))
-  (let ((*dynamic-environment* *dynamic-environment*))
+  (let* ((*dynamic-environment* *dynamic-environment*)
+         (tag-ast (ico:tag-ast ast))
+         (tag (interpret-ast client environment tag-ast)))
     (push (make-instance 'catch-entry
-            :name (ico:tag-ast ast)
+            :name tag
             :unwinder (lambda (values)
-                        (throw (ico:tag-ast ast)
+                        (throw tag
                           (apply #'values values))))
           *dynamic-environment*)
-    (catch (ico:tag-ast ast)
+    (catch tag
       (interpret-implicit-progn-asts
        client environment (ico:form-asts ast)))))
--- AST-interpreter/throw-ast.lisp
+++ AST-interpreter/throw-ast.lisp
@@ -1,7 +1,9 @@
 (cl:in-package #:common-boot-ast-interpreter)
 
 (defmethod interpret-ast (client environment (ast ico:throw-ast))
-  (let ((entry (do-throw (ico:tag-ast ast) *dynamic-environment*)))
+  (let* ((tag-ast (ico:tag-ast ast))
+         (tag (interpret-ast client environment tag-ast))
+         (entry (do-throw tag *dynamic-environment*)))
     (funcall (unwinder entry)
              (multiple-value-list
               (interpret-ast client environment (ico:form-ast ast))))))
