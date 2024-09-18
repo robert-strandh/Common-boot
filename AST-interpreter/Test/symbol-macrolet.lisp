80ac94455cb6d6af63f908deded007484e2e8a35
Robert Strandh <robert.strandh@gmail.com>
2024-09-18 15:39:31 +0200

    Add test for references SYMBOL-MACROLET.

---
+++ AST-interpreter/Test/symbol-macrolet.lisp
@@ -0,0 +1,17 @@
+(cl:in-package #:common-boot-ast-interpreter-test)
+
+(define-test symbol-macrolet)
+
+(define-test symbol-macrolet-one-expansion
+  :parent symbol-macrolet
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(symbol-macrolet ((x (+ y 10))) (let ((y 20)) x))
+         (eval-expression client '#1# environment))))
+
+(define-test symbol-macrolet-two-expansions
+  :parent symbol-macrolet
+  (with-default-parameters (client environment global-environment)
+    (iss #1=(symbol-macrolet ((x (+ y 10))
+                              (z (+ x 5)))
+              (let ((y 20)) z))
+         (eval-expression client '#1# environment))))
