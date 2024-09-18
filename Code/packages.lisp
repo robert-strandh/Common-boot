de7ee45b0822a4215aa7591330440c1ba1f5c2d4
Robert Strandh <robert.strandh@gmail.com>
2024-09-18 15:45:35 +0200

    Export COMPILE-LOCAL-MACRO-FUNCTION-AST.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -11,8 +11,7 @@
                     (#:cmd #:common-macro-definitions)
                     (#:ses #:s-expression-syntax)
                     (#:abp #:architecture.builder-protocol)
-                    (#:cst #:concrete-syntax-tree)
-                    (#:cbae #:common-boot-fast-ast-evaluator))
+                    (#:cst #:concrete-syntax-tree))
   (:export #:client
            #:environment
            #:macro-preserving-client
@@ -21,9 +20,11 @@
            #:macro-transforming-client
            #:create-environment
            #:eval-cst
+           #:compile-local-macro-function-ast
            #:eval-expression
            #:cst-to-ast
            #:convert-with-parser-p
            #:convert-with-ordinary-macro-p
            #:no-function-description
+           #:make-builder
            #:name))
