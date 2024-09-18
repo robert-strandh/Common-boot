910503e0900b181ad6475ab2deb1d48db503c3dc
Robert Strandh <robert.strandh@gmail.com>
2024-09-18 15:47:01 +0200

    Fix incorrect test for EVAL-WHEN situations.

--- AST-interpreter/eval-when-ast.lisp
+++ AST-interpreter/eval-when-ast.lisp
@@ -2,8 +2,9 @@
 
 (defmethod interpret-ast (client environment (ast ico:eval-when-ast))
   (let* ((situation-asts (ico:situation-asts ast))
-         (situations (mapcar #'ico:name situation-asts)))
-    (when (or (member ':compile-toplevel situations)
-              (member 'compile situations))
+         (situation-csts (mapcar #'ico:name situation-asts))
+         (situations (mapcar #'cst:raw situation-csts)))
+    (when (or (member ':execute situations)
+              (member 'eval situations))
       (interpret-implicit-progn-asts
        client environment (ico:form-asts ast)))))
