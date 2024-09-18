379b6d56e04bc8fbc6e5c61a53fccfcfb1174f91
Robert Strandh <robert.strandh@gmail.com>
2024-09-18 15:46:25 +0200

    Bind *GLOBAL-ENVIRONMENT* in closures.

--- AST-interpreter/labels-ast.lisp
+++ AST-interpreter/labels-ast.lisp
@@ -86,29 +86,33 @@
 
 (defun interpret-local-function-ast-components
     (client environment lambda-list-ast form-asts)
-  (lambda (&rest arguments)
-    (let ((new-environment environment)
-          (remaining-arguments arguments)
-          (required-section-ast (ico:required-section-ast lambda-list-ast))
-          (optional-section-ast (ico:optional-section-ast lambda-list-ast))
-          (rest-section-ast (ico:rest-section-ast lambda-list-ast))
-          (key-section-ast (ico:key-section-ast lambda-list-ast)))
-      (multiple-value-setq (remaining-arguments new-environment)
-        (handle-required-parameters
-         required-section-ast remaining-arguments new-environment))
-      (multiple-value-setq (remaining-arguments new-environment)
-        (handle-optional-parameters
-         optional-section-ast remaining-arguments new-environment))
-      (unless (null rest-section-ast)
-        (push (cons (ico:name-ast (ico:parameter-ast rest-section-ast))
-                    remaining-arguments)
-              new-environment))
-      (unless (null key-section-ast)
-        (assert (evenp (length remaining-arguments)))
-        (setq new-environment
-              (handle-key-parameters
-               key-section-ast remaining-arguments new-environment)))
-      (interpret-implicit-progn-asts client new-environment form-asts))))
+  (let ((global-environment *global-environment*))
+    (lambda (&rest arguments)
+      (let ((*global-environment* global-environment)
+            (new-environment environment)
+            (remaining-arguments arguments)
+            (required-section-ast (ico:required-section-ast lambda-list-ast))
+            (optional-section-ast (ico:optional-section-ast lambda-list-ast))
+            (rest-section-ast (ico:rest-section-ast lambda-list-ast))
+            (key-section-ast (ico:key-section-ast lambda-list-ast)))
+        (unless (null required-section-ast)
+          (multiple-value-setq (remaining-arguments new-environment)
+            (handle-required-parameters
+             required-section-ast remaining-arguments new-environment)))
+        (unless (null optional-section-ast)
+          (multiple-value-setq (remaining-arguments new-environment)
+            (handle-optional-parameters
+             optional-section-ast remaining-arguments new-environment)))
+        (unless (null rest-section-ast)
+          (push (cons (ico:name-ast (ico:parameter-ast rest-section-ast))
+                      remaining-arguments)
+                new-environment))
+        (unless (null key-section-ast)
+          (assert (evenp (length remaining-arguments)))
+          (setq new-environment
+                (handle-key-parameters
+                 key-section-ast remaining-arguments new-environment)))
+        (interpret-implicit-progn-asts client new-environment form-asts)))))
     
 (defmethod interpret-ast (client environment (ast ico:labels-ast))
   (let ((new-environment environment))
@@ -130,3 +134,25 @@
     ;; environment.
     (interpret-implicit-progn-asts
      client new-environment (ico:form-asts ast))))
+
+(defmethod cb:compile-local-macro-function-ast
+    ((client client) local-function-ast environment)
+  (let ((simplified-ast (simplify-ast local-function-ast)))
+    (let* ((lambda-list-ast (ico:lambda-list-ast simplified-ast))
+           (required-section-ast (ico:required-section-ast lambda-list-ast))
+           (parameter-asts (ico:parameter-asts required-section-ast))
+           (form-parameter-ast (first parameter-asts))
+           (form-variable-definition-ast
+             (ico:name-ast form-parameter-ast))
+           (environment-parameter-ast (second parameter-asts))
+           (environment-variable-definition-ast
+             (ico:name-ast environment-parameter-ast)))
+    (lambda ()
+      (lambda (form env)
+        (let ((lexical-environment
+                (list
+                 (cons form-variable-definition-ast form)
+                 (cons environment-variable-definition-ast env)))
+              (*global-environment* environment))
+          (interpret-implicit-progn-asts
+           client lexical-environment (ico:form-asts simplified-ast))))))))
