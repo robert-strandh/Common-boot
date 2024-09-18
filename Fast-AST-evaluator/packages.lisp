b88f7ad7f583e1274d3b98dcde6c5c9b913b5a55
Robert Strandh <robert.strandh@gmail.com>
2024-09-18 15:41:34 +0200

    No longer export COMPILE-LOCAL-MACRO-FUNCTION-AST

--- Fast-AST-evaluator/packages.lisp
+++ Fast-AST-evaluator/packages.lisp
@@ -5,12 +5,13 @@
   (:local-nicknames (#:ico #:iconoclast)
                     (#:iat #:iconoclast-ast-transformations)
                     (#:clo #:clostrum)
+                    (#:cb #:common-boot)
                     (#:cm #:common-macros))
   (:shadow #:boundp
            #:symbol-value)
-  (:export #:eval-expression
+  (:export #:client
+           #:eval-expression
            #:compile-ast
-           #:compile-local-macro-function-ast
            #:boundp
            #:symbol-value
            #:origin
