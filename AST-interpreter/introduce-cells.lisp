b9594bfb005cf995a72ada50584aee11aa41195e
Robert Strandh <robert.strandh@gmail.com>
2024-09-20 14:24:45 +0200

    New component for introducting ASTs with cells.

---
+++ AST-interpreter/introduce-cells.lisp
@@ -0,0 +1,35 @@
+(cl:in-package #:common-boot-ast-interpreter)
+
+(defclass global-function-cell-ast (ico:ast)
+  ((%cell :initarg :cell :reader cell)))
+
+(defclass introduce-global-function-cells-client
+    (trucler-reference:client)
+  ((%environment :initarg :environment :reader environment)
+   (%client :initarg :client :reader client)))
+
+(defun introduce-global-function-cells-helper (igfc-client ast)
+  (let* ((environment (environment igfc-client))
+         (client (client igfc-client))
+         (name (ico:name ast))
+         (global-environment (trucler:global-environment client environment))
+         (cell (clostrum:ensure-operator-cell
+                client global-environment name)))
+    (change-class ast 'global-function-cell-ast :cell cell)))
+
+(defmethod iaw:walk-ast-node :around
+    ((igfc-client introduce-global-function-cells-client)
+     (ast ico:global-function-name-reference-ast))
+  ;; Start by converting any children of this AST node.
+  (call-next-method)
+  (let ()
+    (introduce-global-function-cells-helper igfc-client ast)))
+
+(defun introduce-global-function-cells (client environment ast)
+  (let ((igfc-client (make-instance 'introduce-global-function-cells-client
+                       :environment environment
+                       :client client)))
+    (iaw:walk-ast igfc-client ast)))
+
+(defun introduce-cells (client environment ast)
+  (introduce-global-function-cells client environment ast))
