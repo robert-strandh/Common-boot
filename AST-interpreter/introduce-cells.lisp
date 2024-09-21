6ca6283162c4e15fb0884baac40189099112939b
Robert Strandh <robert.strandh@gmail.com>
2024-09-21 16:13:35 +0200

    Rename to simplify and prepare for other cell types.

--- AST-interpreter/introduce-cells.lisp
+++ AST-interpreter/introduce-cells.lisp
@@ -3,11 +3,11 @@
 (defclass global-function-cell-ast (ico:ast)
   ((%cell :initarg :cell :reader cell)))
 
-(defclass introduce-global-function-cells-client ()
+(defclass introduce-cells-client ()
   ((%environment :initarg :environment :reader environment)
    (%client :initarg :client :reader client)))
 
-(defun introduce-global-function-cells-helper (igfc-client ast)
+(defun introduce-cells-helper (igfc-client ast)
   (let* ((global-environment (environment igfc-client))
          (client (client igfc-client))
          (name (ico:name ast))
@@ -16,18 +16,15 @@
     (change-class ast 'global-function-cell-ast :cell cell)))
 
 (defmethod iaw:walk-ast-node :around
-    ((igfc-client introduce-global-function-cells-client)
+    ((igfc-client introduce-cells-client)
      (ast ico:global-function-name-reference-ast))
   ;; Start by converting any children of this AST node.
   (call-next-method)
   (let ()
-    (introduce-global-function-cells-helper igfc-client ast)))
+    (introduce-cells-helper igfc-client ast)))
 
-(defun introduce-global-function-cells (client environment ast)
-  (let ((igfc-client (make-instance 'introduce-global-function-cells-client
+(defun introduce-cells (client environment ast)
+  (let ((igfc-client (make-instance 'introduce-cells-client
                        :environment environment
                        :client client)))
     (iaw:walk-ast igfc-client ast)))
-
-(defun introduce-cells (client environment ast)
-  (introduce-global-function-cells client environment ast))
