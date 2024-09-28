44e8fde80d755d34f3b5ee617e5b9a383773c050
Robert Strandh <robert.strandh@gmail.com>
2024-09-28 13:46:07 +0200

    Fix incorrect handling of BLOCK/RETURN-FROM.

--- AST-interpreter/block-ast.lisp
+++ AST-interpreter/block-ast.lisp
@@ -1,13 +1,15 @@
 (cl:in-package #:common-boot-ast-interpreter)
 
 (defmethod interpret-ast (client environment (ast ico:block-ast))
-  (let ((*dynamic-environment* *dynamic-environment*)
-        (catch-tag (gensym)))
+  (let* ((*dynamic-environment* *dynamic-environment*)
+         (catch-tag (gensym))
+         (identity (list nil))
+         (new-environment (acons (ico:name-ast ast) identity environment)))
     (push (make-instance 'block-entry
-            :name (ico:name-ast ast)
+            :name identity
             :unwinder (lambda (values)
                         (throw catch-tag (apply #'values values))))
           *dynamic-environment*)
     (catch catch-tag
       (interpret-implicit-progn-asts
-       client environment (ico:form-asts ast)))))
+       client new-environment (ico:form-asts ast)))))
--- AST-interpreter/return-from-ast.lisp
+++ AST-interpreter/return-from-ast.lisp
@@ -3,7 +3,8 @@
 (defmethod interpret-ast (client environment (ast ico:return-from-ast))
   (let* ((name-ast (ico:name-ast ast))
          (definition-ast (ico:block-name-definition-ast name-ast))
-         (entry (do-return-from definition-ast *dynamic-environment*)))
+         (identity (lookup definition-ast environment))
+         (entry (do-return-from identity *dynamic-environment*)))
     (funcall
      (unwinder entry)
      (if (null (ico:form-ast ast))
