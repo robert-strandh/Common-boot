7c0ca6c0fde37993a1ed1092aaef0cdc4a4de10e
Robert Strandh <robert.strandh@gmail.com>
2024-09-30 16:26:08 +0200

    Create identity and vector of unwinders.

--- AST-interpreter/tagbody-ast.lisp
+++ AST-interpreter/tagbody-ast.lisp
@@ -3,24 +3,26 @@
 (defmethod interpret-ast (client environment (ast ico:tagbody-ast))
   (let* ((*dynamic-environment* *dynamic-environment*)
          (catch-tag (gensym))
+         (identity (list nil))
+         (new-environment (acons ast identity environment))
          (segment-asts (ico:segment-asts ast))
          (remaining-segment-asts segment-asts))
     (push (make-instance 'tagbody-entry
-            :tag-entries
-            (loop for rest-asts on remaining-segment-asts
-                  for first-ast = (first rest-asts)
-                  unless (null (ico:tag-ast first-ast))
-                    collect (let ((rest-asts rest-asts)
-                                  (tag-ast (ico:tag-ast first-ast)))
-                              (make-instance 'tag-entry
-                                :name tag-ast
-                                :unwinder (lambda ()
-                                            (setq remaining-segment-asts
-                                                  rest-asts)
-                                            (throw catch-tag nil))))))
+            :identity identity
+            :unwinders
+            (coerce 
+             (loop for rest-asts on remaining-segment-asts
+                   for first-ast = (first rest-asts)
+                   unless (null (ico:tag-ast first-ast))
+                     collect (let ((rest-asts rest-asts))
+                               (lambda ()
+                                 (setq remaining-segment-asts
+                                       rest-asts)
+                                 (throw catch-tag nil))))
+             'vector))
           *dynamic-environment*)
     (loop until (null remaining-segment-asts)
           do (catch catch-tag
                (interpret-ast
-                client environment (first remaining-segment-asts))
+                client new-environment (first remaining-segment-asts))
                (pop remaining-segment-asts)))))
