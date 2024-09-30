867a935301f19bcc14335ef4428fcdccfbffcd92
Robert Strandh <robert.strandh@gmail.com>
2024-09-30 16:26:35 +0200

    Find the unwinder of the entry and call it.

--- AST-interpreter/go-ast.lisp
+++ AST-interpreter/go-ast.lisp
@@ -3,5 +3,15 @@
 (defmethod interpret-ast (client environment (ast ico:go-ast))
   (let* ((tag-reference-ast (ico:tag-ast ast))
          (tag-definition-ast (ico:tag-definition-ast tag-reference-ast))
-         (entry (do-go tag-definition-ast *dynamic-environment*)))
-    (funcall (unwinder entry))))
+         (segment-ast (ico:segment-ast tag-definition-ast))
+         (tagbody-ast (ico:tagbody-ast segment-ast))
+         (identity (lookup tagbody-ast environment))
+         (segment-asts (ico:segment-asts tagbody-ast))
+         (relevant-segment-asts
+           (if (null (ico:tag-ast (first segment-asts)))
+               (rest segment-asts)
+               segment-asts))
+         (entry (do-go identity *dynamic-environment*))
+         (index (position tag-definition-ast relevant-segment-asts
+                          :test #'eq :key #'ico:tag-ast)))
+    (funcall (aref (unwinders entry) index))))
