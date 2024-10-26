bf7d2103dae23e9581cae36f124835c7112c42f7
Robert Strandh <robert.strandh@gmail.com>
2024-10-26 14:49:58 +0200

    Include closure conversion is AST simplification.

--- AST-interpreter/eval.lisp
+++ AST-interpreter/eval.lisp
@@ -16,7 +16,7 @@
          (ast (iat:convert-tagbody ast))
          (ast (iat:transform-function-definition-and-reference ast))
          (ast (iat:eliminate-function ast))
-         #+(or)(ast (iat:closure-conversion ast)))
+         (ast (iat:closure-conversion ast)))
     ast))
 
 (defun interpret (client ast)
