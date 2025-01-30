2fe348fbeb5c508100824ba946330b2123e410eb
Robert Strandh <robert.strandh@gmail.com>
2025-01-30 14:25:07 +0100

    Fix incorrect code.

--- Fast-AST-evaluator/find-local-functions.lisp
+++ Fast-AST-evaluator/find-local-functions.lisp
@@ -6,13 +6,11 @@
 (defmethod iaw:walk-ast-node :around
     ((client find-local-functions-client) (ast ico:labels-ast))
   (call-next-method)
-  (loop for binding-ast in (ico:binding-asts ast)
-        do (push (cons (ico:lambda-list-ast binding-ast)
-                       (ico:form-asts binding-ast))
-                 (local-functions client)))
+  (setf (local-functions client)
+        (append (ico:binding-asts ast) (local-functions client)))
   ast)
 
-(defun find-local-functions (ast)
+(defun find-local-function-asts (ast)
   (let ((client (make-instance 'find-local-functions-client)))
     (iaw:walk-ast client ast)
     (local-functions client)))
