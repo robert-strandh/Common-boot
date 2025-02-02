a35792f7fea7565ee20fc30645b00665de0c0f20
Robert Strandh <robert.strandh@gmail.com>
2025-02-02 11:23:54 +0100

    Fix incorrect environment manipulation.

--- Fast-AST-evaluator/block-with-variable-ast.lisp
+++ Fast-AST-evaluator/block-with-variable-ast.lisp
@@ -3,9 +3,11 @@
 (defmethod translate-ast
     (client (ast ico:block-with-variable-ast))
   (let* ((catch-tag (gensym))
-         (identity (list nil)))
-    (setf (lookup (ico:variable-definition-ast ast)) identity)
-    `(let ((dynamic-environment dynamic-environment))
+         (identity (list nil))
+         (host-variable (gensym)))
+    (setf (lookup (ico:variable-definition-ast ast)) host-variable)
+    `(let ((dynamic-environment dynamic-environment)
+           (,host-variable ',identity))
        (declare (ignorable dynamic-environment))
        (push (make-instance 'block-entry
                :name ',identity
