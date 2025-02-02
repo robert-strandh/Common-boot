c8b750a97c87a4a34f7e5ab586fdd0a197a44b11
Robert Strandh <robert.strandh@gmail.com>
2025-02-02 18:20:03 +0100

    Generate the catch tag at run time.

--- Fast-AST-evaluator/block-with-variable-ast.lisp
+++ Fast-AST-evaluator/block-with-variable-ast.lisp
@@ -2,17 +2,19 @@
 
 (defmethod translate-ast
     (client (ast ico:block-with-variable-ast))
-  (let* ((catch-tag (gensym))
+  (let* ((catch-tag-variable (gensym))
          (host-variable (gensym)))
     (setf (lookup (ico:variable-definition-ast ast)) host-variable)
     `(let ((dynamic-environment dynamic-environment)
+           (,catch-tag-variable (list nil))
            (,host-variable (list nil)))
        (declare (ignorable dynamic-environment))
        (push (make-instance 'block-entry
                :name ,host-variable
                :unwinder (lambda (values)
-                           (throw ',catch-tag (apply #'values values))))
+                           (throw ,catch-tag-variable
+                             (apply #'values values))))
              dynamic-environment)
-       (catch ',catch-tag
+       (catch ,catch-tag-variable
          ,@(translate-implicit-progn
             client (ico:form-asts ast))))))
