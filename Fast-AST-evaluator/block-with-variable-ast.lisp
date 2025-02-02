f54a39c902919f792f5a7440bb08e81911deacd3
Robert Strandh <robert.strandh@gmail.com>
2025-02-02 14:00:28 +0100

    Create the identity at run time.

--- Fast-AST-evaluator/block-with-variable-ast.lisp
+++ Fast-AST-evaluator/block-with-variable-ast.lisp
@@ -3,14 +3,13 @@
 (defmethod translate-ast
     (client (ast ico:block-with-variable-ast))
   (let* ((catch-tag (gensym))
-         (identity (list nil))
          (host-variable (gensym)))
     (setf (lookup (ico:variable-definition-ast ast)) host-variable)
     `(let ((dynamic-environment dynamic-environment)
-           (,host-variable ',identity))
+           (,host-variable (list nil)))
        (declare (ignorable dynamic-environment))
        (push (make-instance 'block-entry
-               :name ',identity
+               :name ,host-variable
                :unwinder (lambda (values)
                            (throw ',catch-tag (apply #'values values))))
              dynamic-environment)
