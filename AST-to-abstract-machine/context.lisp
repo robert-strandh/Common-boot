291f1d2ed99547aab6302438242b0770363a9f75
Robert Strandh <robert.strandh@gmail.com>
2025-02-08 17:49:26 +0100

    Define special variables *NEXT-REGISTER-NUMBER*, *REGISTER-NUMBERS*.

--- AST-to-abstract-machine/context.lisp
+++ AST-to-abstract-machine/context.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:common-boot-ast-to-abstract-machine)
 
+(defvar *register-numbers*)
+
+(defvar *next-register-number*)
+
 (defclass context ()
-  ((%next-instruction :initarg :next-instruction :reader next-instruction)
-   (%values-count :initarg :values-count :reader values-count)))
+  ((%next-instruction
+    :initarg :next-instruction
+    :reader next-instruction)
+   (%values-count
+    :initarg :values-count
+    :reader values-count)))
