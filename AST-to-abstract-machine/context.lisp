84f64c0e231ea4e1253af6eb59b9d53464a515b5
Robert Strandh <robert.strandh@gmail.com>
2025-02-09 09:04:41 +0100

    Add slot TARGET-REGISTER.

--- AST-to-abstract-machine/context.lisp
+++ AST-to-abstract-machine/context.lisp
@@ -8,6 +8,10 @@
   ((%next-instruction
     :initarg :next-instruction
     :reader next-instruction)
+   ;; This slot contains 0, 1, or :ALL.
    (%values-count
     :initarg :values-count
-    :reader values-count)))
+    :reader values-count)
+   (%target-register
+    :initarg :target-register
+    :reader target-register)))
