a11e243c2ed31e736518f56c4bb8d912dcd5d577
Robert Strandh <robert.strandh@gmail.com>
2025-02-09 13:58:31 +0100

    Finish code for method specialized to LET-TEMPORARY-AST.

--- AST-to-abstract-machine/let-temporary-ast.lisp
+++ AST-to-abstract-machine/let-temporary-ast.lisp
@@ -4,6 +4,11 @@
   (let* ((binding-ast (ico:binding-ast ast))
          (variable-definition-ast (ico:variable-name-ast binding-ast))
          (form-ast (ico:form-ast binding-ast))
-         (register (new-register)))
-    (assign-register variable-definition-ast register)
-    (translate-ast client context form-ast)))
+         (form-asts (ico:form-asts ast)))
+    (with-new-register (variable-definition-ast register)
+      (let* ((body-code
+              (translate-implicit-progn client context form-asts))
+             (new-context
+               (make-context (next-instructions body-code) 1 register)))
+        (translate-ast client new-context form-ast)))))
+
