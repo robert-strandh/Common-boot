800c13960c96f67dfca8434c7343d2868ed5fc2f
Robert Strandh <robert.strandh@gmail.com>
2025-02-09 13:08:41 +0100

    Bind *DYNAMIC-ENVIRONMENT-REGISTER* and improve RETURN instruction.

--- AST-to-abstract-machine/translate.lisp
+++ AST-to-abstract-machine/translate.lisp
@@ -5,9 +5,10 @@
 (defun translate (client ast)
   (let* ((*register-numbers* (make-hash-table :test #'eq))
          (*next-register-number* 0)
+         (*dynamic-environment-register* (new-register))
          (target-register (new-register))
          (context (make-instance 'context
-                    :next-instruction '(return)
+                    :next-instruction `(return ,target-register)
                     :values-count :all
                     :target-register target-register)))
     (translate-ast client context ast)))
