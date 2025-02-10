07b123e07304acf676b5f5a4416cb55b40966248
Robert Strandh <robert.strandh@gmail.com>
2025-02-10 15:56:30 +0100

    Use new macro WITH-CONTEXT-COMPONENTS.

--- AST-to-abstract-machine/make-cell-ast.lisp
+++ AST-to-abstract-machine/make-cell-ast.lisp
@@ -1,15 +1,13 @@
 (cl:in-package #:common-boot-ast-to-abstract-machine)
 
 (defmethod translate-ast (client context (ast ico:make-cell-ast))
-  (let ((target-register (target-register context))
-        (next-instructions (next-instructions context))
-        (values-count (values-count context))
-        (form-ast (ico:form-ast ast)))
+  (with-context-components (context)
     (assert (eql values-count 1))
-    (with-new-register (nil register)
-      (let ((new-context
-              (make-context 
-               (cons `(make-cell ,target-register ,register)
-                     next-instructions)
-               1 register)))
-        (translate-ast client new-context form-ast)))))
+    (let ((form-ast (ico:form-ast ast)))
+      (with-new-register (nil register)
+        (let ((new-context
+                (make-context 
+                 (cons `(make-cell ,target-register ,register)
+                       next-instructions)
+                 1 register)))
+          (translate-ast client new-context form-ast))))))
