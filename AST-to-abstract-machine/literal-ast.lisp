b7970f34cef3e8c62b38cc4f17b64ecf9e6c156c
Robert Strandh <robert.strandh@gmail.com>
2025-02-10 15:59:05 +0100

    Simplify method by using new macro WITH-CONTEXT-COMPONENTS.

--- AST-to-abstract-machine/literal-ast.lisp
+++ AST-to-abstract-machine/literal-ast.lisp
@@ -1,13 +1,11 @@
 (cl:in-package #:common-boot-ast-to-abstract-machine)
 
 (defmethod translate-ast (client context (ast ico:literal-ast))
-  (let ((target-register (target-register context))
-        (next-instructions (next-instructions context))
-        (values-count (values-count context))
-        (literal (ico:literal ast)))
-    (ecase values-count
-      (0 next-instructions)
-      (1 (cons `(literal ,target-register ,literal)
-               next-instructions))
-      (:all (cons `(literal-list ,target-register ,literal)
-                  next-instructions)))))
+  (with-context-components (context)
+    (let ((literal (ico:literal ast)))
+      (ecase values-count
+        (0 next-instructions)
+        (1 (cons `(literal ,target-register ,literal)
+                 next-instructions))
+        (:all (cons `(literal-list ,target-register ,literal)
+                    next-instructions))))))
