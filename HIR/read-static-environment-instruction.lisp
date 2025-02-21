25bf31096d2394069aca295c631afb9d17d0f118
Robert Strandh <robert.strandh@gmail.com>
2025-02-21 06:36:55 +0100

    Modify comment to reflect recently removed instructoin class.

--- HIR/read-static-environment-instruction.lisp
+++ HIR/read-static-environment-instruction.lisp
@@ -1,8 +1,7 @@
 (cl:in-package #:common-boot-hir)
 
 ;;; This instruction has two inputs and one output.  The first input
-;;; is a static-environment object as output by
-;;; STATIC-ENVIRONMENT-INSTRUCTION, and the second input is a literal
+;;; is a static-environment object and the second input is a literal
 ;;; non-negative integer that indicates the entry number in the static
 ;;; environment to read.  The output is the contents of that entry in
 ;;; the static-environment object.
