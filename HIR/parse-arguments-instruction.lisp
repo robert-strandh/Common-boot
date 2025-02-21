935c116306d392379a56e86b1ce7c0c2c5f734b3
Robert Strandh <robert.strandh@gmail.com>
2025-02-21 06:37:18 +0100

    Add slots for static and dynamic environment.  Improve comment.

--- HIR/parse-arguments-instruction.lisp
+++ HIR/parse-arguments-instruction.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:common-boot-hir)
 
+;;; The LAMBDA-LIST slot contains a "lexified lambda list" as defined
+;;; by the AST transformation LEXIFY-LAMBDA-LIST in Iconoclast, except
+;;; that the VARIABLE-ASTs have been replaced by registers.
+
 ;;; This instruction has only outputs.  It has one output for each
 ;;; required parameter.  It has two outputs for each &OPTIONAL
 ;;; parameter, one for the parameter itself and one for the SUPPLIED-P
@@ -11,4 +15,10 @@
 (defclass parse-arguments-instruction (instruction)
   ((%lambda-list
     :initarg :lambda-list
-    :reader lambda-list)))
+    :reader lambda-list)
+   (%static-environment-register
+    :initarg :static-environment-register
+    :reader static-environment-register)
+   (%dynamic-environment-register
+    :initarg :dynamic-environment-register
+    :reader dynamic-environment-register)))
