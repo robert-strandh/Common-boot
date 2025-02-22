58080274872aee42103adeb4bbea518b35749961
Robert Strandh <robert.strandh@gmail.com>
2025-02-22 09:11:04 +0100

    New comonent for method on SET-STATIC-ENVIRONMENT-INSTRUCTION.

---
+++ HIR-evaluator/set-static-environment-instruction.lisp
@@ -0,0 +1,26 @@
+(cl:in-package #:common-boot-hir-evaluator)
+
+(defmethod instruction-thunk
+    (client
+     (instruction hir:set-static-environment-instruction)
+     lexical-environment)
+  (let* ((inputs (hir:inputs instruction))
+         (closure-object-input (first inputs))
+         (closure-object-lexical-location
+           (ensure-lexical-reference
+            closure-object-input lexical-environment))
+         (static-environment-element-inputs (rest inputs))
+         (static-environment-lexical-locations
+           (loop for input in static-environment-element-inputs
+                 collect (ensure-lexical-reference
+                          input lexical-environment)))
+         (static-environment-size
+           (length static-environment-element-inputs)))
+    (lambda (lexical-locations)
+      (let ((static-environment (make-array static-environment-size)))
+        (loop for lexical-location in static-environment-lexical-locations
+              do (setf (svref static-environment lexical-location)
+                       (lexical-value lexical-locations lexical-location)))
+        (setf (lexical-value
+               lexical-locations closure-object-lexical-location)
+              static-environment)))))
