a0e3c1ffe6a7c090ebfad12e537e0a4c34e8b2bf
Robert Strandh <robert.strandh@gmail.com>
2025-02-22 12:18:30 +0100

    Set *UNWIND-VALUES* no NIL after use to avoid memory leak.

--- HIR-evaluator/receive-instruction.lisp
+++ HIR-evaluator/receive-instruction.lisp
@@ -7,4 +7,5 @@
   (make-thunk (client instruction lexical-environment
                :inputs 0 :outputs 1)
     (setf (output 0) *unwind-values*)
+    (setf *unwind-values* nil)
     (successor 0)))
