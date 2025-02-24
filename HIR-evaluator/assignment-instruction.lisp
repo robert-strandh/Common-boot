d44c1bef68c484819f46a4d062c4596183394b82
Robert Strandh <robert.strandh@gmail.com>
2025-02-24 05:23:47 +0100

    Account for the input being a literal.

--- HIR-evaluator/assignment-instruction.lisp
+++ HIR-evaluator/assignment-instruction.lisp
@@ -1,24 +1,29 @@
 (cl:in-package #:common-boot-hir-evaluator)
 
-(defmethod instruction-thunk
+(defmethod ensure-thunk
     (client
      (instruction hir:assignment-instruction)
      lexical-environment)
   (let ((input-register (first (hir:inputs instruction)))
         (output-register (first (hir:outputs instruction))))
-    (cond ((eq (class-of input-register) (class-of output-register))
-           (make-thunk (client instruction lexical-environment
-                        :inputs 1 :outputs 1)
-             (setf (output 0) (input 0))
-             (successor 0)))
-          ((typep input-register 'hir:single-value-register)
-           (make-thunk (client instruction lexical-environment
-                        :inputs 1 :outputs 1)
-             (setf (output 0) (list (input 0)))
-             (successor 0)))
+    (cond ((typep output-register 'hir:multiple-value-register)
+           (if (typep input-register 'hir:multiple-value-register)
+               (make-thunk (client instruction lexical-environment
+                            :inputs 1 :outputs 1)
+                 (setf (output 0) (input 0))
+                 (successor 0))
+               (make-thunk (client instruction lexical-environment
+                            :inputs 1 :outputs 1)
+                 (setf (output 0) (list (input 0)))
+                 (successor 0))))
           (t
-           (make-thunk (client instruction lexical-environment
-                        :inputs 1 :outputs 1)
-             ;; We rely on the fact that (CAR NIL) => NIL.
-             (setf (output 0) (car (input 0)))
-             (successor 0))))))
+           (if (typep input-register 'hir:multiple-value-register)
+               (make-thunk (client instruction lexical-environment
+                            :inputs 1 :outputs 1)
+                 ;; We rely on the fact that (CAR NIL) => NIL.
+                 (setf (output 0) (car (input 0)))
+                 (successor 0))
+               (make-thunk (client instruction lexical-environment
+                            :inputs 1 :outputs 1)
+                 (setf (output 0) (input 0))
+                 (successor 0)))))))
