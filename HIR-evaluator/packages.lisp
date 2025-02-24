0c6d091c67dea394041ea228a51027917ce02bfc
Robert Strandh <robert.strandh@gmail.com>
2025-02-24 05:26:31 +0100

    Introduce more package-local nicknames.

--- HIR-evaluator/packages.lisp
+++ HIR-evaluator/packages.lisp
@@ -2,5 +2,7 @@
 
 (defpackage #:common-boot-hir-evaluator
   (:use #:common-lisp)
-  (:local-nicknames (#:hir #:common-boot-hir))
-  (:export))
+  (:local-nicknames (#:hir #:common-boot-hir)
+                    (#:cb #:common-boot)
+                    (#:cbah #:common-boot-ast-to-hir))
+  (:export #:client))
