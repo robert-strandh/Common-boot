849d63d0a01abcf2f0a8e2d435af387be330a0c4
Robert Strandh <robert.strandh@gmail.com>
2025-02-26 06:51:22 +0100

    Bind UNWIND-TAG otside thunk.

--- HIR-evaluator/exit-point-instruction.lisp
+++ HIR-evaluator/exit-point-instruction.lisp
@@ -4,14 +4,14 @@
     (client
      (instruction hir:exit-point-instruction)
      lexical-environment)
-  (make-thunk (client instruction lexical-environment
-               :inputs 1 :outputs 2 :successors 1)
-    (let ((unique-identity (list nil))
-          (unwind-tag *unwind-tag*))
-      (setf (output 0) unique-identity)
-      (setf (output 1)
-            (cons (make-instance 'exit-point-entry
-                    :unwind-tag unwind-tag
-                    :unique-identity unique-identity)
-                  (input 0))))
-    (successor 0)))
+  (let ((unwind-tag *unwind-tag*))
+    (make-thunk (client instruction lexical-environment
+                 :inputs 1 :outputs 2 :successors 1)
+      (let ((unique-identity (list nil)))
+        (setf (output 0) unique-identity)
+        (setf (output 1)
+              (cons (make-instance 'exit-point-entry
+                      :unwind-tag unwind-tag
+                      :unique-identity unique-identity)
+                    (input 0))))
+      (successor 0))))
