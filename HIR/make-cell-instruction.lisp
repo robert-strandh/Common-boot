0df8f78ccd85da9a90e66ef3fd45c66f156adfdd
Robert Strandh <robert.strandh@gmail.com>
2025-02-27 06:36:26 +0100

    Fix incorrect comment.

--- HIR/make-cell-instruction.lisp
+++ HIR/make-cell-instruction.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has no inputs.  It has a single output.  The
-;;; output is a new "cell" the representation of which is not
+;;; This instruction has one input.  The input is a value to be stored
+;;; in the cell created by this instruction.  It has a single output.
+;;; The output is a new "cell" the representation of which is not
 ;;; determined by this library.
 
 (defclass make-cell-instruction (instruction)
