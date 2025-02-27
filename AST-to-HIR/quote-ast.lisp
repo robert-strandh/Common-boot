55ae8776dd3548529ce7211a2989578694aae501
Robert Strandh <robert.strandh@gmail.com>
2025-02-27 07:40:56 +0100

    Fix incorrect code for accessing raw object.

--- AST-to-HIR/quote-ast.lisp
+++ AST-to-HIR/quote-ast.lisp
@@ -2,8 +2,8 @@
 
 (defmethod translate-ast (client (ast ico:quote-ast))
   (let* ((object-ast (ico:object-ast ast))
-         (form-ast (ico:form-ast object-ast))
-         (object (cst:raw form-ast)))
+         (form (ico:form object-ast))
+         (object (cst:raw form)))
     (make-instance 'hir:assignment-instruction
       :inputs (list (make-instance 'hir:literal :value object))
       :outputs (list *target-register*)
