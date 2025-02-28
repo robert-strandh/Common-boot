61269af65efccc1ecafcd679db48f466146d4db7
Robert Strandh <robert.strandh@gmail.com>
2025-02-28 05:59:20 +0100

    Use register adapter to simplify method for VARIABLE-REFERENCE-AST.

--- AST-to-HIR/variable-reference-ast.lisp
+++ AST-to-HIR/variable-reference-ast.lisp
@@ -3,7 +3,9 @@
 (defmethod translate-ast (client (ast ico:variable-reference-ast))
   (let* ((variable-definition-ast (ico:definition-ast ast))
          (register (find-register variable-definition-ast)))
-    (make-instance 'hir:assignment-instruction
-      :inputs (list register)
-      :outputs (list *target-register*)
-      :successors (list *next-instruction*))))
+    (multiple-value-bind (*next-instruction* *target-register*)
+        (adapt-register 'hir:single-value-register)
+      (make-instance 'hir:assignment-instruction
+        :inputs (list register)
+        :outputs (list *target-register*)
+        :successors (list *next-instruction*)))))
