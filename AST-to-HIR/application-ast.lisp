0c01b2ecfd504c8b0813bb7e99bb486108ec6e25
Robert Strandh <robert.strandh@gmail.com>
2025-02-28 05:31:40 +0100

    Use new register adapter to simplify code.

--- AST-to-HIR/application-ast.lisp
+++ AST-to-HIR/application-ast.lisp
@@ -5,17 +5,19 @@
          (argument-asts (ico:argument-asts ast))
          (registers
            (loop repeat (1+ (length argument-asts))
-                 collect (make-instance 'hir:single-value-register)))
-         (result
-           (make-instance 'hir:funcall-instruction
-             :origin (ico:origin ast)
-             :inputs (cons *dynamic-environment-register* registers)
-             :outputs (list *target-register*)
-             :successors (list *next-instruction*))))
-    (loop for ast in (reverse (cons function-name-ast argument-asts))
-          for register in (reverse registers)
-          do (let ((*next-instruction* result)
-                   (*target-register* register))
-               (setf result
-                     (translate-ast client ast))))
-    result))
+                 collect (make-instance 'hir:single-value-register))))
+    (multiple-value-bind (*next-instruction* *target-register*)
+        (adapt-register 'hir:multiple-value-register)
+      (let ((result
+              (make-instance 'hir:funcall-instruction
+                :origin (ico:origin ast)
+                :inputs (cons *dynamic-environment-register* registers)
+                :outputs (list *target-register*)
+                :successors (list *next-instruction*))))
+        (loop for ast in (reverse (cons function-name-ast argument-asts))
+              for register in (reverse registers)
+              do (let ((*next-instruction* result)
+                       (*target-register* register))
+                   (setf result
+                         (translate-ast client ast))))
+        result))))
