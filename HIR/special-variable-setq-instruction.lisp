28e71cebf63ceb68cb082051cf3f56bbebd80033
Robert Strandh <robert.strandh@gmail.com>
2025-03-01 11:07:01 +0100

    Change so that the variable name is a slot in the instruction.

--- HIR/special-variable-setq-instruction.lisp
+++ HIR/special-variable-setq-instruction.lisp
@@ -1,8 +1,11 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has two inputs.  The first input is a symbol
-;;; naming the special variable to assign to.  The second input is the
-;;; value to assign.  This instruction has no outputs.
+;;; This instruction has two inputs.  The first input is a
+;;; dynamic-environment object.  The second input is the value to
+;;; assign.  This instruction has one output which is the new value of
+;;; the special variable.
 
 (defclass special-variable-setq-instruction (instruction)
-  ())
+  ((%variable-name
+    :initarg :variable-name
+    :reader variable-name)))
