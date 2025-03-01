8139310a732848fbb4320a6bc94000d1aafadf78
Robert Strandh <robert.strandh@gmail.com>
2025-03-01 14:41:09 +0100

    Fix inorrect code order.

--- AST-to-HIR/progv-ast.lisp
+++ AST-to-HIR/progv-ast.lisp
@@ -5,7 +5,7 @@
            *dynamic-environment-register*)
          (new-dynamic-environment-register
            (make-instance 'hir:single-value-register
-             :name "dynamic env"))
+             :name "new dynamic env"))
          (symbols-register
            (make-instance 'hir:single-value-register))
          (values-register
@@ -14,18 +14,18 @@
            new-dynamic-environment-register)
          (*next-instruction*
            (translate-implicit-progn client (ico:form-asts ast)))
+         (*next-instruction*
+           (make-instance 'hir:progv-instruction
+             :origin (ico:origin ast)
+             :inputs (list existing-dynamic-environment-register
+                           symbols-register
+                           values-register)
+             :outputs (list new-dynamic-environment-register)
+             :successors (list *next-instruction*)))
          (*dynamic-environment-register*
            existing-dynamic-environment-register)
          (*target-register* values-register)
          (*next-instruction*
            (translate-ast client (ico:values-ast ast)))
-         (*target-register* symbols-register)
-         (*next-instruction*
-           (translate-ast client (ico:symbols-ast ast))))
-    (make-instance 'hir:progv-instruction
-      :origin (ico:origin ast)
-      :inputs (list existing-dynamic-environment-register
-                    symbols-register
-                    values-register)
-      :outputs (list new-dynamic-environment-register)
-      :successors (list *next-instruction*))))
+         (*target-register* symbols-register))
+    (translate-ast client (ico:symbols-ast ast))))
