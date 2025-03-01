c8d9dc12fc2ff97806b8f15e47dccdf74441c826
Robert Strandh <robert.strandh@gmail.com>
2025-03-01 11:07:29 +0100

    Add an assignment instruction for the output.

--- AST-to-HIR/special-variable-setq-ast.lisp
+++ AST-to-HIR/special-variable-setq-ast.lisp
@@ -1,16 +1,18 @@
 (cl:in-package #:common-boot-ast-to-hir)
 
 (defmethod translate-ast (client (ast ico:special-variable-setq-ast))
-  (let* ((symbol-register (make-instance 'hir:single-value-register))
-         (value-register (make-instance 'hir:single-value-register))
+  (let* ((value-register (make-instance 'hir:single-value-register))
          (*next-instruction*
-           (make-instance 'hir:special-variable-setq-instruction
-             :origin (ico:origin ast)
-             :inputs (list symbol-register value-register)
-             :outputs '()
+           (make-instance 'hir:assignment-instruction
+             :inputs (list value-register)
+             :outputs (list *target-register*)
              :successors (list *next-instruction*)))
-         (*target-register* value-register)
          (*next-instruction*
-           (translate-ast client (ico:value-ast ast)))
-         (*target-register* symbol-register))
-    (translate-ast client (ico:variable-name-ast ast))))
+              (make-instance 'hir:special-variable-setq-instruction
+                :origin (ico:origin ast)
+                :variable-name (ico:name (ico:variable-name-ast ast))
+                :inputs (list *dynamic-environment-register* value-register)
+                :outputs '()
+                :successors (list *next-instruction*)))
+         (*target-register* value-register))
+    (translate-ast client (ico:value-ast ast))))
