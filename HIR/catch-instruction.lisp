9d2f91dac0cf5eb0f85c1b6aa079cbd47df73625
Robert Strandh <robert.strandh@gmail.com>
2025-03-02 10:39:22 +0100

    Update comment according to recent changes.

--- HIR/catch-instruction.lisp
+++ HIR/catch-instruction.lisp
@@ -1,12 +1,11 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has two inputs, two outputs, and two successors.
+;;; This instruction has two inputs, one output, and two successors.
 ;;; The first input is a dynamic-environment object. The second input
-;;; is the catch tag.  The first output is a dynamic-environment
-;;; object.  The second output is the target register for the CATCH
-;;; form.  The first successor is the first instruction of the body of
-;;; the CATCH form.  That successor is used when the CATCH instruction
-;;; is entered normally.  The second successor is the successor of the
+;;; is the catch tag.  The output is a dynamic-environment object.
+;;; The first successor is the first instruction of the body of the
+;;; CATCH form.  That successor is used when the CATCH instruction is
+;;; entered normally.  The second successor is the successor of the
 ;;; CATCH form.  That successor is used when the CATCH form is entered
 ;;; through a THROW.
 
