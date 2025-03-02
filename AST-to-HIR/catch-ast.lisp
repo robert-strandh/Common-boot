728c6b8dac0da3e3f15dd9ad71e0f29c80072929
Robert Strandh <robert.strandh@gmail.com>
2025-03-02 10:40:04 +0100

    Introduce a RECEIVE-INSTRUCTION.

--- AST-to-HIR/catch-ast.lisp
+++ AST-to-HIR/catch-ast.lisp
@@ -14,12 +14,16 @@
          (*dynamic-environment-register* new-dynamic-environment-register)
          (body-instruction
            (translate-implicit-progn client (ico:form-asts ast)))
+         (receive-instruction
+           (make-instance 'hir:receive-instruction
+             :inputs '()
+             :outputs (list target-register)
+             :successors (list next-instruction)))
          (*next-instruction*
            (make-instance 'hir:catch-instruction
              :origin (ico:origin ast)
              :inputs (list existing-dynamic-environment-register
-                      tag-register)
-             :outputs (list new-dynamic-environment-register
-                       target-register)
-             :successors (list body-instruction next-instruction))))
-    (translate-ast client (ico:tag-ast ast))))
+                           tag-register)
+             :outputs (list new-dynamic-environment-register)
+             :successors (list body-instruction receive-instruction))))
+    (translate-ast client (ico:tag-form-ast ast))))
