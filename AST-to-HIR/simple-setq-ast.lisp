12f1c55ef8db4ac233fc7617fdeffc1c2560d34d
Robert Strandh <robert.strandh@gmail.com>
2025-03-02 14:03:53 +0100

    Assign the value of the SETQ to the target register.

--- AST-to-HIR/simple-setq-ast.lisp
+++ AST-to-HIR/simple-setq-ast.lisp
@@ -4,5 +4,11 @@
   (let* ((value-ast (ico:value-ast ast))
          (variable-name-ast (ico:variable-name-ast ast))
          (definition-ast (ico:definition-ast variable-name-ast))
-         (*target-register* (find-register definition-ast)))
+         (register-to-assign-to (find-register definition-ast))
+         (*next-instruction*
+           (make-instance 'hir:assignment-instruction
+             :inputs (list register-to-assign-to)
+             :outputs (list *target-register*)
+             :successors (list *next-instruction*)))
+         (*target-register* register-to-assign-to))
     (translate-ast client value-ast)))
