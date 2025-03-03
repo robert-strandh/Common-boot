38961296f8af6ae1ad797a34dd27f5bd8c2e067c
Robert Strandh <robert.strandh@gmail.com>
2025-03-03 17:15:34 +0100

    Define method on COMPILE-LOCAL-MACRO-FUNCTION-AST.

--- HIR-evaluator/eval.lisp
+++ HIR-evaluator/eval.lisp
@@ -14,3 +14,25 @@
          (*environment* (trucler:global-environment client environment))
          (function (top-level-hir-to-host-function client hir)))
     (funcall function)))
+
+(defmethod cb:compile-local-macro-function-ast
+    ((client client) local-function-ast environment)
+  (let* ((function-definition-ast
+           (ico:name-ast local-function-ast))
+         (function-reference-ast
+           (make-instance 'ico:function-reference-ast
+             :name (ico:name function-definition-ast)
+             :definition-ast function-definition-ast))
+         (function-ast
+           (make-instance 'ico:function-ast
+             :name-ast function-reference-ast))
+         (labels-ast
+           (make-instance 'ico:labels-ast
+             :binding-asts (list local-function-ast)
+             :form-asts (list function-ast))))
+    (reinitialize-instance function-definition-ast
+      :reference-asts (list function-reference-ast))
+    (let* ((hir (cbah:ast-to-hir client labels-ast))
+           (*environment* (trucler:global-environment client environment))
+           (function (top-level-hir-to-host-function client hir)))
+      function)))
