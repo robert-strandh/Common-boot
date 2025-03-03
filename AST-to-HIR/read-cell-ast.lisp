d047199408ae03f7942ab03440dec869bec3e37a
Robert Strandh <robert.strandh@gmail.com>
2025-03-03 09:41:35 +0100

    Use the register adapter.

--- AST-to-HIR/read-cell-ast.lisp
+++ AST-to-HIR/read-cell-ast.lisp
@@ -1,11 +1,13 @@
 (cl:in-package #:common-boot-ast-to-hir)
 
 (defmethod translate-ast (client (ast ico:read-cell-ast))
-  (let* ((register (make-instance 'hir:single-value-register))
-         (*next-instruction*
-           (make-instance 'hir:read-cell-instruction
-             :inputs (list register)
-             :outputs (list *target-register*)
-             :successors (list *next-instruction*)))
-         (*target-register* register))
-    (translate-ast client (ico:cell-ast ast))))
+  (let ((register (make-instance 'hir:single-value-register)))
+    (multiple-value-bind (*next-instruction* *target-register*)
+        (adapt-register 'hir:single-value-register)
+      (let* ((*next-instruction*
+               (make-instance 'hir:read-cell-instruction
+                 :inputs (list register)
+                 :outputs (list *target-register*)
+                 :successors (list *next-instruction*)))
+             (*target-register* register))
+        (translate-ast client (ico:cell-ast ast))))))
