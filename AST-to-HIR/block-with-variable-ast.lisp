3130e1edcd8c49ba1f8c9222193cd5852f342019
Robert Strandh <robert.strandh@gmail.com>
2025-03-09 09:16:27 +0100

    Fix incorrect comments and incorrect code.

--- AST-to-HIR/block-with-variable-ast.lisp
+++ AST-to-HIR/block-with-variable-ast.lisp
@@ -1,13 +1,13 @@
 (cl:in-package #:common-boot-ast-to-hir)
 
 ;;; This variable holds an association list that maps
-;;; VARIABLE-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to
+;;; BLOCK-NAME-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to
 ;;; RECEIVE-INSTRUCTIONs to be used by UNWIND-INSTRUCTIONs resulting
 ;;; from the translation of RETURN-FROM-WITH-VARIABLE-AST
 (defparameter *block-receive-instruction* '())
 
 ;;; This variable holds an association list that maps
-;;; VARIABLE-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to to a
+;;; BLOCK-NAME-DEFINITION-ASTs of BLOCK-WITH-VARIABLE-ASTs to to a
 ;;; target register (which can be NIL), but only the class of the
 ;;; value is used, to encode the number of values required.  The value
 ;;; of this variable is to be used to create a new register of the
@@ -17,6 +17,7 @@
 
 (defmethod translate-ast (client (ast ico:block-with-variable-ast))
   (let ((variable-definition-ast (ico:variable-definition-ast ast))
+        (definition-ast (ico:name-ast ast))
         (identity-register (make-instance 'hir:single-value-register)))
     (setf (find-register variable-definition-ast) identity-register)
     (let* ((receive-instruction
@@ -25,8 +26,12 @@
                             '()
                             (list *target-register*))
                :successors (list *next-instruction*)))
-           (*block-receive-instruction* receive-instruction)
-           (*block-target-register* *target-register*)
+           (*block-receive-instruction*
+             (acons definition-ast receive-instruction
+                    *block-receive-instruction*))
+           (*block-target-register*
+             (acons definition-ast *target-register*
+                    *block-target-register*))
            (current-dynamic-environment-register
              *dynamic-environment-register*)
            (*dynamic-environment-register*
