3fcfbf23d2a8681ee7b7da8968f77806c27d95f9
Robert Strandh <robert.strandh@gmail.com>
2025-03-09 09:16:51 +0100

    Look up block target register and receive instruction in alist.

--- AST-to-HIR/return-from-with-variable-ast.lisp
+++ AST-to-HIR/return-from-with-variable-ast.lisp
@@ -2,14 +2,21 @@
 
 (defmethod translate-ast (client (ast ico:return-from-with-variable-ast))
   (let* ((variable-reference-ast (ico:variable-reference-ast ast))
-         (definition-ast (ico:definition-ast variable-reference-ast))
-         (identity-register (find-register definition-ast))
+         (variable-definition-ast
+           (ico:definition-ast variable-reference-ast))
+         (block-reference-ast (ico:name-ast ast))
+         (block-definition-ast
+           (ico:block-name-definition-ast block-reference-ast))
+         (identity-register (find-register variable-definition-ast))
          (form-ast (if (null (ico:form-ast ast))
                        (make-instance 'ico:literal-ast
                          :origin (ico:origin ast)
                          :literal nil)
                        (ico:form-ast ast)))
-         (block-target-register *block-target-register*)
+         (block-target-register
+           (cdr (assoc block-definition-ast *block-target-register*)))
+         (block-receive-instruction
+           (cdr (assoc block-definition-ast *block-receive-instruction*)))
          (*target-register*
            (if (null block-target-register)
                nil
@@ -23,5 +30,5 @@
                                 '()
                                 (list *target-register*)))
              :outputs '()
-             :successors (list *block-receive-instruction*))))
+             :successors (list block-receive-instruction))))
     (translate-ast client form-ast)))
