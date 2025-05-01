e878edb11ac771211f55a837dc1cfefe017312ca
Robert Strandh <robert.strandh@gmail.com>
2025-05-01 15:19:35 +0200

    Define documentatio strings for ASSIGNMENT-INSTRUCTION.

--- HIR/assignment-instruction.lisp
+++ HIR/assignment-instruction.lisp
@@ -2,3 +2,19 @@
 
 (defclass assignment-instruction (instruction)
   ())
+
+(setf (documentation 'assignment-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               assignment-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has one input and one~@
+               output.  The input is a register or a literal.  The~@
+               output is a register.~@
+               ~@
+               An instruction of this type has a single successor.~@
+               ~@
+               The result of executing and instruction of this type~@
+               is that the register of the output will contain the~@
+               object of the literal or the register of the input."))
+
