4d59e77da8ee257d61e06a05b0bbe86fc6bc84c3
Robert Strandh <robert.strandh@gmail.com>
2025-05-01 17:05:13 +0200

    Convert comment into a documentation string.

--- HIR/read-cell-instruction.lisp
+++ HIR/read-cell-instruction.lisp
@@ -1,9 +1,20 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has one input and one output.  The input is a
-;;; register containing a cell, as created by the
-;;; MAKE-CELL-INSTRUCTION.  The output is a register that will contain
-;;; the contents of the cell.
-
 (defclass read-cell-instruction (instruction)
   ())
+
+(setf (documentation 'read-cell-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               read-cell-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has one input and one~@
+               output.  The input is a register containing a cell,~@
+               as created by the MAKE-CELL-INSTRUCTION.  The output~@
+               is a register.~@
+               ~@
+               An instruction of this type has a single successor.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that the output register will contain the contents~@
+               of the cell."))
