74867c2921cd3168572314d4bc6c88b0e545645c
Robert Strandh <robert.strandh@gmail.com>
2025-05-01 14:58:34 +0200

    Move comment to documentation string.

--- HIR/write-cell-instruction.lisp
+++ HIR/write-cell-instruction.lisp
@@ -1,10 +1,18 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has two inputs and no outputs.  The first input
-;;; is a register containing a cell as created by the
-;;; MAKE-CELL-INSTRUCTION.  The second input is a register or a
-;;; literal.  The result of executing this instruction is that the
-;;; cell will contain the object in the second input.
-
 (defclass write-cell-instruction (instruction)
   ())
+
+(setf (documentation 'write-cell-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               write-cell-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has two inputs and no~@
+               outputs.  The first input is a register containing~@
+               a cell as created by the MAKE-CELL-INSTRUCTION.~@
+               The second input is a register or a literal.
+               ~@
+               The result of executing and instruction of this type~@
+               is that the cell will contain the object in the second~@
+               input."))
