98a79232fb85c989c71f58b92d6c95721e29bc32
Robert Strandh <robert.strandh@gmail.com>
2025-05-02 11:31:10 +0200

    Convert comment into a documentation string for CATCH-INSTRUCTION.

--- HIR/catch-instruction.lisp
+++ HIR/catch-instruction.lisp
@@ -1,13 +1,34 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has two inputs, one output, and two successors.
-;;; The first input is a dynamic-environment object. The second input
-;;; is the catch tag.  The output is a dynamic-environment object.
-;;; The first successor is the first instruction of the body of the
-;;; CATCH form.  That successor is used when the CATCH instruction is
-;;; entered normally.  The second successor is the successor of the
-;;; CATCH form.  That successor is used when the CATCH form is entered
-;;; through a THROW.
-
 (defclass catch-instruction (instruction)
   ())
+
+(setf (documentation 'catch-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               catch-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has two inputs and one~@
+               output.  The first input is a register containing a~@
+               dynamic-environment object.  The second input is a~@
+               register or a literal containing a catch tag.  The~@
+               output is a register.
+               ~@
+               An instruction of this type has two successors.  The~@
+               first successor is the instruction corresponding to~@
+               the first form of the body of the CATCH form.  The~@
+               second successor is the instruction corresponding to~@
+               the form to be evaluated after the CATCH form.~@
+               ~@
+               When the CATCH instruction is entered normally, the~@
+               output register will contain a dynamic-environment~@
+               object which is like the one in the first input, except~@
+               that it has been augmented with an entry corresponding~@
+               to the CATCH form.  The first successor is then chosen.~@
+               ~@
+               The second successor is chosen when the CATCH form is~@
+               entered through the corresponding THROW. 
+               ~@
+               The output register should not be used by any the second~@
+               successor of an instruction of this type, nor by any of~@
+               its successors."))
