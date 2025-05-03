adf13e0803d432c6a8bb3db5f320fa590dc307bc
Robert Strandh <robert.strandh@gmail.com>
2025-05-03 10:15:21 +0200

    Convert comment into a documentation string.  Add more information.

--- HIR/if-instruction.lisp
+++ HIR/if-instruction.lisp
@@ -1,8 +1,20 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has one input, no outputs, and two successors.
-;;; If the input is true, then the first successor is chosen,
-;;; otherwise the second successor is chosen.
-
 (defclass if-instruction (instruction)
   ())
+
+(setf (documentation 'if-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               if-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has one input and no~@
+               outputs.  The input is a register or a literal.~@
+               ~@
+               An instruction of this type has two successors.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that the value of input is examined, and if it~@
+               is an object other than NIL, then the first successor~@
+               is chosen.  If it is the object NIL, then the second~@
+               successor is chosen."))
