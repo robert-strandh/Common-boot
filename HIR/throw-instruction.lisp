71ea2b4bbfc04f020b9f152f1b5e7741543e1384
Robert Strandh <robert.strandh@gmail.com>
2025-05-03 14:56:55 +0200

    Convert comment into a documentation string.  Add more information.

--- HIR/throw-instruction.lisp
+++ HIR/throw-instruction.lisp
@@ -1,9 +1,22 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instructin has three inputs, no outputs, and no successors.
-;;; The first input is a dynamic-environment object.  The second input
-;;; is a catch tag.  The third input is the list of values to be
-;;; thrown.
-
 (defclass throw-instruction (instruction)
   ())
+
+(setf (documentation 'throw-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               throw-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has three inputs and no~@
+               outputs.  The first input is a register containing~@
+               a dynamic-environment object.  The second input is a~@
+               register or a literal holding a catch tag.  The third~@
+               input is a register or a literal, containing the list~@
+               of values to be thrown.~@
+               ~@
+               An instruction of this type has no successors.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that the values are transmitted to the corresponding~@
+               CATCH-INSTRUCTION."))
