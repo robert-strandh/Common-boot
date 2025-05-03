31ee039688dfae17c5bbbd04f1dfe5c3f0d84a4b
Robert Strandh <robert.strandh@gmail.com>
2025-05-03 14:18:54 +0200

    Convert comment into a documentation string.

--- HIR/nop-instruction.lisp
+++ HIR/nop-instruction.lisp
@@ -1,6 +1,16 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has no inputs and no outputs.  It does nothing.
-
 (defclass nop-instruction (instruction)
   ())
+
+(setf (documentation 'nop-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               nop-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has no inputs and no~@
+               outputs.
+               ~@
+               An instruction of this type has a single successor~@
+               ~@
+               Executing an instruction of this time has no effect."))
