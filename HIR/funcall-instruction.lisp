f6ef98bb08b3b7b44d7e0450a41ba5df8379430e
Robert Strandh <robert.strandh@gmail.com>
2025-05-03 10:07:50 +0200

    Convert comment into a documentation string.  Add more information.

--- HIR/funcall-instruction.lisp
+++ HIR/funcall-instruction.lisp
@@ -1,11 +1,27 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has at least two inputs and it has one output.
-;;; The first input is a function object to be called.  The second
-;;; input is the dynamic-environment object representing the current
-;;; dynamic environment.  The remaining inputs are the arguments to be
-;;; transferred to the function.  The output is the value (possibly
-;;; multiple values) returned from the instruction.
-
 (defclass funcall-instruction (instruction)
   ())
+
+(setf (documentation 'funcall-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               funcall-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has at least two inputs,~@
+               and it has one output.  The first input is a register~@
+               contains the function object to be called.  The second~@
+               input is a register that contains a dynamic-environment~@
+               object.  Each of the remaining inputs is either a~@
+               register or a literal, and these remaining inputs are~@
+               the arguments to be passed to the callee.  The output~@
+               is a register that can be a multiple-value register.~@
+               ~@
+               And instruction of this type has a single successor.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that the function will be called with the arguments~@
+               in the remaining inputs, and with a dynamic environment~@
+               that is the value of the second input.  The values~@
+               returned by the function will be contained in the output~@
+               register."))
