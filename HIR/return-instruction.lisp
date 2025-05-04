947f638b4f65296c3d125a096112e8a04efadbd6
Robert Strandh <robert.strandh@gmail.com>
2025-05-04 14:35:21 +0200

    Convert comment into a documentation string.  Add more information.

--- HIR/return-instruction.lisp
+++ HIR/return-instruction.lisp
@@ -1,7 +1,19 @@
 (cl:in-package #:common-boot-hir)
 
-;;; This instruction has a single input which is a
-;;; MULTIPLE-VALUE-REGISTER.  It has no successors.
-
 (defclass return-instruction (instruction)
   ())
+
+(setf (documentation 'return-instruction 'type)
+      (format nil
+              "Class precedence list:~@
+               return-instruction, instruction, standard-object, t~@
+               ~@
+               An instruction of this type has one input and no~@
+               outputs.  The input is a MULTIPLE-VALUE-REGISTER.~@
+               ~@
+               An instruction of this type has no successors.~@
+               ~@
+               The result of executing an instruction of this type~@
+               is that control is transferred to the caller of this~@
+               function, with the objects in the input register as~@
+               the return values."))
